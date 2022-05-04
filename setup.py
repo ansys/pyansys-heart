6e3d427f3994c64c7c76bc3cae9264b7ac4d584c
kelhouar <karim.elhouari@ansys.com>
2022-05-04 15:56:32 +0200

    change name in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 setup(
-    name="ansys-heart-lib",
+    name="ansys-heart",
     version="0.1.dev0",
     url="https://github.com/pyansys/pyheart",
     author="ANSYS, Inc.",

8dfa7fe183cf7f688c9c5518ebb697d1d712ff2c
ansmhoeij <102799582+ansmhoeij@users.noreply.github.com>
2022-05-31 09:54:02 +0200

    Replace custom segset add (#4)
    * Replaced custom segment set add kw
    
    * Removed obsolete import

--- ansys/heart/writer/keyword_module.py
+++ ansys/heart/writer/keyword_module.py
@@ -13,11 +13,6 @@ from ansys.dyna.keywords import Deck
 from ansys.heart.writer.custom_dynalib_keywords._custom_set_node_list import (
     SetNodeList_custom,
 )
-from ansys.heart.writer.custom_dynalib_keywords._custom_set_segment_add import (
-    SetSegmentAdd_custom,
-)
-
-
 
 
 def create_node_keyword(nodes: np.array, offset: int = 0) -> keywords.Node:
@@ -171,32 +166,6 @@ def create_node_set_keyword(
     kw.nodes = df
 
     return kw
-
-
-def create_segment_set_add_keyword(
-    segment_ids: np.array, set_id: int = 1, title: str = "segment-set-title"
-) -> SetSegmentAdd_custom:
-    """Creates a SetSegmentAdd keyword from a list of segment ids
-    """
-    kw = SetSegmentAdd_custom()
-    kw.sid = set_id
-    kw.options["TITLE"].active = True
-    kw.title = title
-    # rearrange set ids such that its size is n x 8
-    num_seg_ids = len(segment_ids)
-    num_cols = int(8)
-    num_rows = int(np.ceil(num_seg_ids / 8))
-    segment_ids2 = np.zeros(int(num_rows * num_cols)) * np.nan
-    segment_ids2[:num_seg_ids] = segment_ids
-    # reshaped segment_ids with nan values
-    segment_ids2 = np.reshape(segment_ids2, (num_rows, num_cols))
-
-    # set up dataframe
-    df = pd.DataFrame(data=segment_ids2, columns=kw.segment_ids.columns)
-
-    kw.segment_ids = df
-    return kw
-
 
 def create_element_shell_keyword(
     shells: np.array, part_id: int = 1, id_offset: int = 0

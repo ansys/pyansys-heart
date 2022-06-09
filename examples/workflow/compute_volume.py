23cc963772a4f6d86497ff3b20f232f87ceb4c72
wye <wenfeng.ye@ansys.com>
2022-06-09 14:40:55 +0200

    Add a raw method to compute STL's volume

--- examples/workflow/compute_volume.py
+++ examples/workflow/compute_volume.py
@@ -1,7 +1,74 @@
+import os
+
 import numpy as np
 import meshio
 import vtk
 import json
+
+
+def get_mass_properties(stl_obj):
+    """
+    http://www.geometrictools.com/Documentation/PolyhedralMassProperties.pdf
+    """
+
+    def subexpression(x):
+        w0, w1, w2 = x[:, 0], x[:, 1], x[:, 2]
+        temp0 = w0 + w1
+        f1 = temp0 + w2
+        temp1 = w0 * w0
+        temp2 = temp1 + w1 * temp0
+        f2 = temp2 + w2 * f1
+        f3 = w0 * temp1 + w1 * temp2 + w2 * f2
+        g0 = f2 + w0 * (f1 + w0)
+        g1 = f2 + w1 * (f1 + w1)
+        g2 = f2 + w2 * (f1 + w2)
+        return f1, f2, f3, g0, g1, g2
+
+    x0, x1, x2 = stl_obj.x[:, 0], stl_obj.x[:, 1], stl_obj.x[:, 2]
+    y0, y1, y2 = stl_obj.y[:, 0], stl_obj.y[:, 1], stl_obj.y[:, 2]
+    z0, z1, z2 = stl_obj.z[:, 0], stl_obj.z[:, 1], stl_obj.z[:, 2]
+    a1, b1, c1 = x1 - x0, y1 - y0, z1 - z0
+    a2, b2, c2 = x2 - x0, y2 - y0, z2 - z0
+    d0, d1, d2 = b1 * c2 - b2 * c1, a2 * c1 - a1 * c2, a1 * b2 - a2 * b1
+
+    f1x, f2x, f3x, g0x, g1x, g2x = subexpression(stl_obj.x)
+    f1y, f2y, f3y, g0y, g1y, g2y = subexpression(stl_obj.y)
+    f1z, f2z, f3z, g0z, g1z, g2z = subexpression(stl_obj.z)
+
+    intg = np.zeros(10)
+    intg[0] = sum(d0 * f1x)
+    intg[1:4] = sum(d0 * f2x), sum(d1 * f2y), sum(d2 * f2z)
+    intg[4:7] = sum(d0 * f3x), sum(d1 * f3y), sum(d2 * f3z)
+    intg[7] = sum(d0 * (y0 * g0x + y1 * g1x + y2 * g2x))
+    intg[8] = sum(d1 * (z0 * g0y + z1 * g1y + z2 * g2y))
+    intg[9] = sum(d2 * (x0 * g0z + x1 * g1z + x2 * g2z))
+    intg /= np.array([6, 24, 24, 24, 60, 60, 60, 120, 120, 120])
+    volume = intg[0]
+    cog = intg[1:4] / volume
+    cogsq = cog ** 2
+    inertia = np.zeros((3, 3))
+    inertia[0, 0] = intg[5] + intg[6] - volume * (cogsq[1] + cogsq[2])
+    inertia[1, 1] = intg[4] + intg[6] - volume * (cogsq[2] + cogsq[0])
+    inertia[2, 2] = intg[4] + intg[5] - volume * (cogsq[0] + cogsq[1])
+    inertia[0, 1] = inertia[1, 0] = -(intg[7] - volume * cog[0] * cog[1])
+    inertia[1, 2] = inertia[2, 1] = -(intg[8] - volume * cog[1] * cog[2])
+    inertia[0, 2] = inertia[2, 0] = -(intg[9] - volume * cog[2] * cog[0])
+    return volume, cog, inertia
+
+
+class STL:
+    def __init__(self, coord, connect):
+        self.x = coord[connect][:, :, 0]
+        self.y = coord[connect][:, :, 1]
+        self.z = coord[connect][:, :, 2]
+
+
+def get_cavity_volume2(cavity):
+    ids, a = np.unique(cavity, return_inverse=True)
+    coords = x_m[ids]
+    connectivity = a.reshape(cavity.shape)
+    a, _, _ = get_mass_properties(STL(coords, connectivity))
+    return abs(a)
 
 
 def get_cavity_volume(cavity):
@@ -33,10 +100,11 @@ def compute_stl_volume(f_stl):
 if __name__ == "__main__":
     """
     Update the unstressed cavity volume in the system model settings.
-    
+
     How to use:
     Copy and run this script under the main simulation directory
     """
+    os.chdir(r"D:\pyheart-lib\examples\heart\workdir\bi_ventricle_model\lsdyna_files")
 
     nodes_file = "nodes.k"
     # BV or 4C
@@ -51,10 +119,12 @@ if __name__ == "__main__":
                 data.append(line)
     x_m = np.genfromtxt(data, delimiter=[8, 16, 16, 16])[:, 1:4]
 
-    lv_cavity = np.loadtxt(lv_cavity_file,delimiter=',',dtype=int)
-    rv_cavity = np.loadtxt(rv_cavity_file,delimiter=',',dtype=int)
+    lv_cavity = np.loadtxt(lv_cavity_file, delimiter=",", dtype=int)
+    rv_cavity = np.loadtxt(rv_cavity_file, delimiter=",", dtype=int)
     lv_volume = get_cavity_volume(lv_cavity)
     rv_volume = get_cavity_volume(rv_cavity)
+    print(lv_volume)
+    print(rv_volume)
 
     json_file = r"system_model_settings.json"
     with open(json_file) as f:

5b2c44a3b64e0229aab44ddb8b411f3505153df0
M. Hoeijmakers <martijn.hoeijmakers@ansys.com>
2022-06-09 17:02:18 +0200

    Missing or wrong keyword files

--- ansys/heart/writer/custom_dynalib_keywords/custom_keywords.py
+++ ansys/heart/writer/custom_dynalib_keywords/custom_keywords.py
@@ -4,3 +4,5 @@ from .keywords.set_node_list import SetNodeList
 from .keywords.em_control_ep import EmControlEp
 from .keywords.em_ep_createfiberorientation import EmEpCreatefiberorientation
 from .keywords.em_ep_fiberinitial import EmEpFiberinitial
+from .keywords.em_mat_003 import EmMat003
+from .keywords.em_ep_cellmodel_tomek import EmEpCellmodelTomek

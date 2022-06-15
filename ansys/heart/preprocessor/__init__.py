7622f1f26f35415429325dad2f6d90232f93a167
ansmhoeij <102799582+ansmhoeij@users.noreply.github.com>
2022-06-15 16:37:14 +0200

    version check in init (#56)

--- ansys/heart/preprocessor/__init__.py
+++ ansys/heart/preprocessor/__init__.py
@@ -1,5 +1,27 @@
 import os
 
-base_dir = os.getenv("AWP_ROOT212")
-SC_EXE = os.path.join(base_dir, "scdm", "SpaceClaim.exe")
-FLUENT_EXE = os.path.join(base_dir, "fluent", "ntbin", "win64", "fluent.exe")
+if os.name == "nt":
+    supported_versions = ["212", "221"]
+    awp_roots = {
+        ver: os.environ.get(f"AWP_ROOT{ver}", "") for ver in supported_versions
+    }
+    installed_versions = {
+        ver: path for ver, path in awp_roots.items() if path and os.path.isdir(path)
+    }    
+
+elif os.name == "posix":
+    assert False, "Posix not supported yet"
+
+if installed_versions:    
+    # get latest installed version:
+    ansys_dir = installed_versions[ sorted(installed_versions.keys() )[-1] ]
+
+    SC_EXE = os.path.join(ansys_dir, "scdm", "SpaceClaim.exe")
+    FLUENT_EXE = os.path.join(ansys_dir, "fluent", "ntbin", "win64", "fluent.exe")
+    assert os.path.isfile( SC_EXE ), "Spaceclaim not found"
+    assert os.path.isfile( FLUENT_EXE ), "Fluent not found"
+else:
+    assert False, "No valid installations found. Valid Ansys installations include: Ansys %s" % supported_versions
+    exit()
+
+

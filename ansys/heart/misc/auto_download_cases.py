245ee2df23468e2d302cf2c377f27e3dca249b30
ansmhoeij <102799582+ansmhoeij@users.noreply.github.com>
2022-06-20 12:08:38 +0200

    Auto downloads cases (#74)

---
+++ ansys/heart/misc/auto_download_cases.py
@@ -0,0 +1,50 @@
+"""Auto downloads cases from the remote repositories of Strocchi et al 2020, and Cristobal et al 2021
+"""
+from ansys.heart.custom_logging import logger
+import os
+from pathlib import Path, PurePath
+import requests
+import wget
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+URLS = {
+    "Strocchi2020" :
+    {
+        "url": "https://zenodo.org/record/3890034", "num_cases" : 24
+    },
+    "Cristobal2021" :
+    {
+        "url" : "https://zenodo.org/record/4590294", "num_cases" : 20
+    }
+}
+DOWNLOAD_DIR = PurePath.joinpath ( Path( __file__ ).parents[3], "downloads" )
+
+def download_cases():
+    overwrite_previous = False
+    for database_name, subdict in URLS.items():        
+        url = subdict["url"]
+        num_cases = subdict["num_cases"]
+
+        download_dir = PurePath.joinpath( DOWNLOAD_DIR, database_name )
+        if not os.path.isdir(download_dir):
+            os.makedirs( download_dir )
+        for ii in range(1, num_cases):
+            print( "\nDownloading case: {:02d} ...".format(ii) )
+            save_path = os.path.join( download_dir, "{:02d}.tar.gz".format(ii) )
+            if os.path.isfile(save_path):
+                print("File already exists, skipping")
+                continue
+            download_url = "{:}/files/{:02d}.tar.gz?download=1".format( url, ii )
+            wget.download(download_url, save_path)        
+    
+    return
+
+def unzip_cases():
+    """Unzips the downloaded cases
+    """
+    #TODO Needs to be implemented
+
+if __name__ == "__main__":
+    download_cases()
+    print("Protected")

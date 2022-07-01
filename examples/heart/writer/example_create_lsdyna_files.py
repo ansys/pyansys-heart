c1cf7845c0016d672ae0e6a34ad9353d02bba978
M. Hoeijmakers <martijn.hoeijmakers@ansys.com>
2022-07-01 11:34:59 +0200

    Generalized writer example

--- examples/heart/writer/example_create_lsdyna_files.py
+++ examples/heart/writer/example_create_lsdyna_files.py
@@ -10,17 +10,27 @@ from pathlib import Path
 from ansys.heart.preprocessor.heart_model import HeartModel
 from ansys.heart.preprocessor.model_information import ModelInformation
 from ansys.heart.custom_logging import logger
-from ansys.heart.writer.dynawriter import MechanicsDynaWriter
+from ansys.heart.writer.dynawriter import (
+    FiberGenerationDynaWriter,
+    MechanicsDynaWriter,
+    ZeroPressureMechanicsDynaWriter,
+)
 from ansys.heart.general import clean_directory
 
 ABS_BASE_PATH = Path(__file__).parent.absolute()
 
 
-def create_ls_dyna_files(path_to_model_info: str, export_directory: str = None):
-    """Creates the LS-DYNA files for the model specified
-    """
+def create_ls_dyna_files(path_to_model_info: str, writer_type: str, export_directory: str = None):
+    """Creates the LS-DYNA files for the model specified"""
+
+    if writer_type not in ["Mechanics", "ZeroPressure", "FiberGeneration"]:
+        logger.error("Writer type %s not valid" % writer_type)
+        return
+
     if not export_directory:
-        export_directory = os.path.join(Path(path_model_info).parent.absolute(), "lsdyna_files")
+        export_directory = os.path.join(
+            Path(path_to_model_info).parent.absolute(), "lsdyna_files", writer_type.lower()
+        )
 
     clean_directory(export_directory)
 
@@ -31,8 +41,14 @@ def create_ls_dyna_files(path_to_model_info: str, export_directory: str = None):
     # load processed heart model from info file
     heart_model.load_model(path_model_info)
 
-    # create dyna writer object and pass loaded model
-    dyna_writer = MechanicsDynaWriter(heart_model)
+    # create dyna writer based on type of writer requested
+    if writer_type == "Mechanics":
+        dyna_writer = MechanicsDynaWriter(heart_model)
+    elif writer_type == "ZeroPressure":
+        dyna_writer = ZeroPressureMechanicsDynaWriter(heart_model)
+    elif writer_type == "FiberGeneration":
+        dyna_writer = FiberGenerationDynaWriter(heart_model)
+
     dyna_writer.update()
     dyna_writer.export(export_directory)
 
@@ -43,42 +59,13 @@ def create_ls_dyna_files(path_to_model_info: str, export_directory: str = None):
 
 if __name__ == "__main__":
 
-    run_all = True
-    if run_all:
-        models_to_run = [
-            "LeftVentricle",
-            "BiVentricle",
-            "FourChamber",
-            "FourChamberOriginal"
-            ]
-    else:
-        models_to_run = []
+    models_to_run = ["LeftVentricle", "BiVentricle", "FourChamber", "FourChamberOriginal"]
+    database = "Strocchi2020"
 
-    if "LeftVentricle" in models_to_run:
-        # left ventricle model:
+    for model in models_to_run:
         path_model_info = os.path.join(
-            ABS_BASE_PATH, "..", "workdir", "left_ventricle_model", "model_info.json"
+            ABS_BASE_PATH, "..", "workdir", database, model, "model_info.json"
         )
-
-        create_ls_dyna_files(path_model_info)
-
-    if "BiVentricle" in models_to_run:
-        # bi-ventricle model
-        path_model_info = os.path.join(
-            ABS_BASE_PATH, "..", "workdir", "bi_ventricle_model", "model_info.json"
-        )
-        create_ls_dyna_files(path_model_info)
-
-    if "FourChamber" in models_to_run:
-        # four chamber model
-        path_model_info = os.path.join(
-            ABS_BASE_PATH, "..", "workdir", "four_chamber_model", "model_info.json"
-        )
-        create_ls_dyna_files(path_model_info)
-
-    if "FourChamberOriginal" in models_to_run:
-        # four chamber model with original mesh
-        path_model_info = os.path.join(
-            ABS_BASE_PATH, "..", "workdir", "four_chamber_model_original", "model_info.json"
-        )
-        create_ls_dyna_files(path_model_info)
+        create_ls_dyna_files(path_model_info, writer_type="Mechanics")
+        create_ls_dyna_files(path_model_info, writer_type="ZeroPressure")
+        create_ls_dyna_files(path_model_info, writer_type="FiberGeneration")

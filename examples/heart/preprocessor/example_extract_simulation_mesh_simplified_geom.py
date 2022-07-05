71d4fdc19324015225c42a3acde0384689f3c648
M. Hoeijmakers <martijn.hoeijmakers@ansys.com>
2022-07-05 13:10:28 +0200

    Added uvc interpolation

--- examples/heart/preprocessor/example_extract_simulation_mesh_simplified_geom.py
+++ examples/heart/preprocessor/example_extract_simulation_mesh_simplified_geom.py
@@ -11,7 +11,13 @@ from pathlib import Path
 from ansys.heart.preprocessor.heart_model import HeartModel
 from ansys.heart.preprocessor.model_information import ModelInformation
 from ansys.heart.custom_logging import logger
-from ansys.heart.preprocessor.vtk_module import write_vtkdata_to_vtkfile
+from ansys.heart.preprocessor.vtk_module import (
+    read_vtk_unstructuredgrid_file,
+    write_vtkdata_to_vtkfile,
+    vtk_read_mesh_file,
+    vtk_map_continuous_data,
+)
+from vtk.numpy_interface import dataset_adapter as dsa
 
 BASE_WORK_DIR = os.path.join(Path(__file__).parent.absolute(), "..", "workdir")
 
@@ -148,5 +154,27 @@ if __name__ == "__main__":
     run_preprocessor(
         model_type, database_name, path_to_case, work_directory, mesh_size, remesh=True
     )
+
+    ## add uvc point data to simulation mesh
+    path_to_source = os.path.join(
+        Path(__file__).parents[3], "downloads", "Strocchi2020", "05", "05.case"
+    )
+    path_to_target = os.path.join(
+        Path(__file__).parents[3],
+        "downloads",
+        "Strocchi2020_simplified",
+        "workdir",
+        "simulation_mesh.vtk",
+    )
+    source_vtk = vtk_read_mesh_file(path_to_source)
+    target_vtk = vtk_read_mesh_file(path_to_target)
+
+    source_dsa = dsa.WrapDataObject(source_vtk)
+    source_point_data_names = source_dsa.PointData.keys()
+    uvc_array_names = [k for k in source_point_data_names if "uvc_" in k]
+    target_vtk1 = vtk_map_continuous_data(
+        source_vtk, target_vtk, False, array_names_to_include=uvc_array_names
+    )
+    write_vtkdata_to_vtkfile(target_vtk1, path_to_target)
 
     logger.info("** DONE **")

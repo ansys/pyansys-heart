3496b76851b6c5d56f510111b0b437fb0715d27a
M. Hoeijmakers <martijn.hoeijmakers@ansys.com>
2022-07-19 14:27:47 +0200

    Cleanup

--- ansys/heart/custom_logging.py
+++ ansys/heart/custom_logging.py
@@ -1,34 +1,3 @@
-# import logging
-# from time import strftime
-
-
-# class Logger:
-#     def __init__(self) -> None:
-#         pass
-
-
-# strftime("%H")
-
-# # create logger
-# logger = logging.getLogger("pyheart-lib")
-# logger.setLevel(logging.DEBUG)
-
-# # create console handler and set level to debug
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.DEBUG)
-
-# # create formatter
-# # dateformat = '%b %d %Y %H:%M:%S'
-# dateformat = "%Y/%m/%d %H:%M:%S"
-# formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt=dateformat)
-
-# # add formatter to ch
-# ch.setFormatter(formatter)
-
-# # add ch to logger
-# logger.addHandler(ch)
-
-
 """Logging module.
 
 This module supplies a general framework for logging in pyheartlib.

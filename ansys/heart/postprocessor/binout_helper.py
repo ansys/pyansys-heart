d98d93331627021667865c8e007765a2fd869391
wye <wenfeng.ye@ansys.com>
2022-10-12 16:58:27 +0200

    add: Elout reader

--- ansys/heart/postprocessor/binout_helper.py
+++ ansys/heart/postprocessor/binout_helper.py
@@ -11,6 +11,52 @@ except ImportError:
     raise ImportError("qd not found.")
 
 
+class Elout:
+    """Object of element output."""
+
+    def __init__(self, fn):
+        """
+        Init Elout object.
+
+        Parameters
+        ----------
+        fn: binout file from LS-DYNA
+        """
+        self.binout = Binout(fn)
+        self.time = self.binout.read("elout", "solid_hist", "time")
+        self.ids = self.binout.read("elout", "solid_hist", "ids")[0]
+        self.solid_nb = len(self.ids)
+
+    def get_stress(self):
+        """
+        Get stress.
+
+        Returns
+        -------
+        numpy array of time * element * 6 stress.
+        stress-xx   stress-yy   stress-zz   stress-xy   stress-yz   stress-zx
+        """
+        data = self.binout.read("elout", "solid_hist", "data").reshape(len(self.time), -1, 9)
+        if not np.all(data[:, :, 0] == 4) or data.shape[1] != self.solid_nb:
+            raise Exception("Cannot read stress correctly.")
+        else:
+            return data[:, :, 1:7]
+
+    def get_history_variable(self):
+        """
+        Get history variables.
+
+        Returns
+        -------
+        numpy array of time * element * 27 history variables.
+        """
+        data = self.binout.read("elout", "solid_hist", "hist").reshape(len(self.time), -1, 28)
+        if not np.all(data[:, :, 0] == 4) or data.shape[1] != self.solid_nb:
+            raise Exception("Cannot read hv correctly.")
+        else:
+            return data[:, :, 1:]
+
+
 def get_coordinates(binfile: str):
     """
     Read binout file to extract node coordinate.

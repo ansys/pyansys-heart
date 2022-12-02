d01fb265da5c21e6058a007fc792b4f113386b36
M. Hoeijmakers <martijn.hoeijmakers@ansys.com>
2022-12-02 19:44:19 +0100

    Add method to download and untar single case

--- ansys/heart/misc/auto_download_cases.py
+++ ansys/heart/misc/auto_download_cases.py
@@ -8,18 +8,84 @@ and Cristobal et al 2021.
 
 import os
 from pathlib import Path, PurePath
+import warnings
 
 from tqdm import tqdm
-import wget  # type: ignore
+
+try:
+    import wget  # type: ignore
+except (ImportError):
+    warnings.warn("wget not installed but requried. Please install by: pip install wget")
+
 
 URLS = {
     "Strocchi2020": {"url": "https://zenodo.org/record/3890034", "num_cases": 24},
-    "Cristobal2021": {"url": "https://zenodo.org/record/4590294", "num_cases": 20},
+    "Rodero2021": {"url": "https://zenodo.org/record/4590294", "num_cases": 20},
 }
+VALID_DATABASES = list(URLS.keys())
 DOWNLOAD_DIR = PurePath.joinpath(Path(__file__).parents[3], "downloads")
 
+# download_folder = "d:\\development\\pyheart-lib\\pyheart-lib\\downloads"
 
-def download_cases():
+
+def download_case(
+    database: str, case_number: int, download_folder: Path, overwrite: bool = True
+) -> Path:
+    """Download a case from the remote repository
+
+    Parameters
+    ----------
+    database : str
+        name of the database. Either Strocchi2020 or Rodero2021
+    case_number : int
+        case number to download
+    download_folder : Path
+        path to the folder in which to download the case
+
+    Returns
+    -------
+    bool
+        flag indicating whether download and unpacking was successful
+    """
+
+    if database not in VALID_DATABASES:
+        raise ValueError("Databse not valid, please specify valid database: %s" % VALID_DATABASES)
+
+    url = URLS[database]["url"]
+    if case_number > URLS[database]["num_cases"]:
+        raise ValueError(
+            "Database {0} only has {1} cases".format(database, URLS[database]["num_cases"])
+        )
+
+    save_dir = os.path.join(download_folder, "{:>03d}".format(case_number))
+    save_path = os.path.join(save_dir, "{:03d}.tar.gz".format(case_number))
+
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+
+    if not overwrite and os.path.isfile(save_path):
+        warnings.warn("File already exists. Skipping...")
+        return 1
+
+    download_url = "{:}/files/{:02d}.tar.gz?download=1".format(url, case_number)
+
+    wget.download(download_url, save_path)
+
+    return save_path
+
+
+def unzip_case(tar_path: Path):
+    """Untar the downloaded tar-ball."""
+    import glob as glob
+    import tarfile
+
+    tar_ball = tarfile.open(tar_path)
+    tar_dir = os.path.dirname(tar_path)
+    tar_ball.extractall(path=tar_dir)
+    return
+
+
+def download_all_cases():
     """Download cases with URL."""
     overwrite_previous = False
     for database_name, subdict in URLS.items():
@@ -41,22 +107,26 @@ def download_cases():
     return
 
 
-def unzip_cases():
+def unzip_all_cases():
     """Un-tar the downloaded cases."""
     import glob as glob
-    import tarfile
 
     for database_name, subdict in URLS.items():
         download_dir = PurePath.joinpath(DOWNLOAD_DIR, database_name)
         files = glob.glob(os.path.join(download_dir, "*.tar.gz"))
         for file in tqdm(files):
-            tar_ball = tarfile.open(file)
-            tar_ball.extractall(path=download_dir)
+            unzip_case(file)
+
     return
 
 
 if __name__ == "__main__":
-    download_cases()
-    unzip_cases()
+    # download_cases()
+    # unzip_cases()
+
+    save_path = download_case(
+        "Strocchi2020", 3, "d:\\development\\pyheart-lib\\pyheart-lib\\downloads"
+    )
+    unzip_case(save_path)
 
     print("Protected")

25e59ed386ab5cd4a00ea916946aa31971de258c
M. Hoeijmakers <martijn.hoeijmakers@ansys.com>
2022-12-05 13:49:01 +0100

    Add hash function validator

--- ansys/heart/misc/downloader.py
+++ ansys/heart/misc/downloader.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 try:
     import wget  # type: ignore
 except (ImportError):
-    warnings.warn("wget not installed but requried. Please install by: pip install wget")
+    warnings.warn("wget not installed but required. Please install by: pip install wget")
 
 
 URLS = {
@@ -24,8 +24,9 @@ URLS = {
 }
 VALID_DATABASES = list(URLS.keys())
 DOWNLOAD_DIR = PurePath.joinpath(Path(__file__).parents[3], "downloads")
-
-# download_folder = "d:\\development\\pyheart-lib\\pyheart-lib\\downloads"
+PATH_TO_HASHTABLE = PurePath.joinpath(
+    Path(__file__).parents[0], "remote_repo_hash_table_sha256.json"
+)
 
 
 def format_download_urls():
@@ -35,7 +36,7 @@ def format_download_urls():
         download_urls[database_name] = {}
         url = URLS[database_name]["url"]
         num_cases = URLS[database_name]["num_cases"]
-        for case_number in range(1, num_cases+1):
+        for case_number in range(1, num_cases + 1):
             download_urls[database_name][case_number] = "{:}/files/{:02d}.tar.gz?download=1".format(
                 url, case_number
             )
@@ -60,10 +61,18 @@ def download_case(
     -------
     bool
         flag indicating whether download and unpacking was successful
+
+    Examples
+    --------
+    Download case 1 from the public repository (Strocchi2020) of pathological hearts.
+    >>> path_to_case = download_case(database="Strocchi2020", case_bumber=1, download_folder="my/download/folder")
+
+    Download case 1 from the public repository (Rodero2021) of 'healthy' hearts.
+    >>> path_to_case = download_case(database="Rodero2021", case_bumber=1, download_folder="my/download/folder")
     """
 
     if database not in VALID_DATABASES:
-        raise ValueError("Databse not valid, please specify valid database: %s" % VALID_DATABASES)
+        raise ValueError("Database not valid, please specify valid database: %s" % VALID_DATABASES)
 
     url = URLS[database]["url"]
     if case_number > URLS[database]["num_cases"]:
@@ -85,7 +94,39 @@ def download_case(
 
     wget.download(download_url, save_path)
 
+    is_valid_file = validate_hash_sha256(
+        file_path=save_path,
+        database=database,
+        casenumber=case_number,
+        path_hash_table=PATH_TO_HASHTABLE,
+    )
+    assert is_valid_file, "File data integrity can not be validatated."
+
     return save_path
+
+
+def validate_hash_sha256(
+    file_path: Path, database: str, casenumber: int, path_hash_table: Path
+) -> bool:
+    """Check the file's hash function against the expected sha256 hash function."""
+    import hashlib
+    import json
+
+    if os.path.isfile(path_hash_table):
+        fid = open(path_hash_table, "r")
+        sha256_table: dict = json.load(fid)
+        fid.close()
+        # convert strings to ints
+        for key in sha256_table.keys():
+            sha256_table[key] = {int(k): v for k, v in sha256_table[key].items()}
+    else:
+        raise FileExistsError("File does not exist")
+
+    sha256 = hashlib.sha256(open(file_path, "rb").read()).hexdigest()
+    if sha256 == sha256_table[database][casenumber]:
+        return True
+    else:
+        return False
 
 
 def unzip_case(tar_path: Path):
@@ -117,6 +158,8 @@ def download_all_cases():
                 continue
             download_url = "{:}/files/{:02d}.tar.gz?download=1".format(url, ii)
             wget.download(download_url, save_path)
+            file_is_valid = validate_hash_sha256(save_path, database_name, ii, PATH_TO_HASHTABLE)
+            assert file_is_valid, "File's integrity could not be validated."
 
     return
 
@@ -140,7 +183,7 @@ if __name__ == "__main__":
     download_urls = format_download_urls()
 
     save_path = download_case(
-        "Strocchi2020", 3, "d:\\development\\pyheart-lib\\pyheart-lib\\downloads"
+        "Rodero2021", 3, "d:\\development\\pyheart-lib\\pyheart-lib\\downloads"
     )
     unzip_case(save_path)
 

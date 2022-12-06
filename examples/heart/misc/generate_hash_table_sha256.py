908e332e6d1b264ec10e9cd721142cbe6badedef
ansmhoeij <102799582+ansmhoeij@users.noreply.github.com>
2022-12-06 09:59:39 +0100

    176 add methods to download data from public repositories (#177)
    * Add method to download and untar single case
    
    * Formatter for all urls
    
    * Add test to validate download urls
    
    * Rename test
    
    * Add test for hash validator
    
    * Add script to generate a reference hash table
    
    * Add reference hash table of Rodero2021 cases
    
    * Add hash function validator
    
    * Add hashtable to resources
    
    * Style fixes
    
    * Save dir depends on database
    
    * Add method to download asset
    
    * Add option to download asset on the fly
    
    * Cleanup
    
    * Update requirements
    
    * Activate downloader test in yml
    
    * Refactor
    
    * Style fixes
    
    * Minor refactor
    
    * Add strocchi cases to hash table
    
    * Style
    
    * Update workflow example

---
+++ examples/heart/misc/generate_hash_table_sha256.py
@@ -0,0 +1,53 @@
+import hashlib
+import json
+import os
+
+from ansys.heart.misc.downloader import download_case
+
+if __name__ == "__main__":
+    """Creates a reference hash table used to check integrity of data files.
+
+    Note
+    ----
+    Use this script to download all .tar.gz files from both the Strocchi2020 and
+    Rodero2021 public datasets and generate a hash table which is used to
+    verify file integrity.
+    """
+    databases = {"Strocchi2020": 24, "Rodero2021": 20}
+    base_folder = "D:\\development\\pyheart-lib\\pyheart-lib\\downloads"
+    path_to_hash_table = os.path.join(base_folder, "remote_repo_hash_table_sha256.json")
+    if os.path.isfile(path_to_hash_table):
+        # read
+        fid = open(path_to_hash_table, "r")
+        sha256_table: dict = json.load(fid)
+        fid.close()
+        # convert strings to ints
+        for key in sha256_table.keys():
+            sha256_table[key] = {int(k): v for k, v in sha256_table[key].items()}
+    else:
+        sha256_table = {}
+
+    for database in databases:
+        num_cases = databases[database]
+        download_folder = os.path.join(base_folder)
+        try:
+            sha256_table[database]
+        except (KeyError):
+            sha256_table[database] = {}
+
+        for ii in range(1, num_cases + 1):
+            try:
+                sha256_table[database][ii]
+                key_exists = True
+                print("{0}:{1} Entry already exists".format(database, ii))
+                continue
+            except (KeyError):
+                key_exists = False
+
+            print("\nDownloading... %d" % ii)
+            path_to_case = download_case(database, ii, download_folder, validate_hash=False)
+            sha256 = hashlib.sha256(open(path_to_case, "rb").read()).hexdigest()
+            sha256_table[database][ii] = sha256
+
+            with open(path_to_hash_table, "w") as outfile:
+                json.dump(sha256_table, outfile, indent=4, ensure_ascii=True)

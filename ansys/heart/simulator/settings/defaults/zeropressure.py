9c3bd69acd9a548f7fb6191368dfab67ce21faa4
wye <wenfeng.ye@ansys.com>
2023-02-17 19:57:25 +0100

    Change zeropressure settings

--- ansys/heart/simulator/settings/defaults/zeropressure.py
+++ ansys/heart/simulator/settings/defaults/zeropressure.py
@@ -6,8 +6,6 @@ analysis = {
     "end_time": Quantity(1000.0, "ms"),
     "dtmin": Quantity(10.0, "ms"),
     "dtmax": Quantity(100.0, "ms"),
-    "dt_d3plot": Quantity(500.0, "ms"),
-    "dt_icvout": Quantity(1.0, "ms"),
-    "global_damping": Quantity(0.5, "1/ms"),
+    "dt_d3plot": Quantity(100.0, "ms"),
     "dt_nodout": Quantity(200, "ms"),
 }

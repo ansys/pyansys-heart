54c0cff7d28325d459e604cdcfdfb5c10bf96630
wye <wenfeng.ye@ansys.com>
2023-02-28 09:34:47 +0100

    style fix

--- ansys/heart/postprocessor/SystemModelPost.py
+++ ansys/heart/postprocessor/SystemModelPost.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 import json
 import os
 
-
 from ansys.heart.simulator.settings.settings import SimulationSettings
 import matplotlib.pyplot as plt
 import numpy as np
@@ -287,11 +286,11 @@ class SystemModelPost:
 
           It's normal that the cavity volume is slight different.
         """
+        from ansys.heart.postprocessor.binout_helper import IcvOut
+
         try:
-            from ansys.heart.postprocessor.binout_helper import IcvOut
             self.bin = IcvOut(os.path.join(self.dir, "binout"))
         except FileExistsError:
-            from ansys.heart.postprocessor.binout_helper import IcvOut
             self.bin = IcvOut(os.path.join(self.dir, "binout0000"))
 
         self.bin.time /= 1000  # ms ->s

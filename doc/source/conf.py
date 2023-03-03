36987de435a3fccccfddcba06e5eff27567e73c9
kelhouar <karim.elhouari@ansys.com>
2023-03-03 16:54:18 +0100

    tryautodoc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -97,3 +97,68 @@ source_suffix = ".rst"
 
 # The master toctree document.
 master_doc = "index"
+
+# Configuration for Sphinx autoapi
+autoapi_type = "python"
+autoapi_dirs = ["../../ansys/heart"]
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "special-members",
+]
+autoapi_template_dir = "_autoapi_templates"
+suppress_warnings = ["autoapi.python_import_resolution"]
+exclude_patterns = ["_autoapi_templates/index.rst"]
+autoapi_python_use_implicit_namespaces = True
+
+# # Examples gallery customization
+# nbsphinx_execute = "always"
+# nbsphinx_custom_formats = {
+#     ".mystnb": ["jupytext.reads", {"fmt": "mystnb"}],
+# }
+# nbsphinx_thumbnails = {
+#     "examples/01_getting_started/01_math": "_static/thumbnails/101_getting_started.png",
+#     "examples/01_getting_started/02_units": "_static/thumbnails/101_getting_started.png",
+#     "examples/01_getting_started/03_sketching": "_static/thumbnails/101_getting_started.png",
+#     "examples/01_getting_started/04_modeling": "_static/thumbnails/101_getting_started.png",
+#     "examples/02_sketching/basic_usage": "_static/thumbnails/basic_usage.png",
+#     "examples/02_sketching/dynamic_sketch_plane": "_static/thumbnails/dynamic_sketch_plane.png",
+#     "examples/02_sketching/advanced_sketching_gears": "_static/thumbnails/advanced_sketching_gears.png",  # noqa: E501
+#     "examples/03_modeling/add_design_material": "_static/thumbnails/add_design_material.png",
+#     "examples/03_modeling/plate_with_hole": "_static/thumbnails/plate_with_hole.png",
+#     "examples/03_modeling/tessellation_usage": "_static/thumbnails/tessellation_usage.png",
+#     "examples/03_modeling/design_organization": "_static/thumbnails/design_organization.png",
+# }
+# nbsphinx_epilog = """
+# ----
+# .. admonition:: Download this example!
+#     Download this example as a `Jupyter Notebook <{cname_pref}/{ipynb_file_loc}>`_
+#     or as a `Python script <{cname_pref}/{py_file_loc}>`_ from the previous links.
+# """.format(
+#     cname_pref=f"https://{cname}/version/{switcher_version}",
+#     ipynb_file_loc="{{ env.docname }}.ipynb",
+#     py_file_loc="{{ env.docname }}.py",
+# )
+
+# nbsphinx_prolog = """
+# .. admonition:: Download this example!
+#     Download this example as a `Jupyter Notebook <{cname_pref}/{ipynb_file_loc}>`_
+#     or as a `Python script <{cname_pref}/{py_file_loc}>`_ from the previous links.
+# ----
+# """.format(
+#     cname_pref=f"https://{cname}/version/{switcher_version}",
+#     ipynb_file_loc="{{ env.docname }}.ipynb",
+#     py_file_loc="{{ env.docname }}.py",
+# )
+
+# typehints_defaults = "comma"
+# simplify_optional_unions = False
+
+# # additional logos for the latex coverpage
+# latex_additional_files = [watermark, ansys_logo_white, ansys_logo_white_cropped]
+
+# # change the preamble of latex with customized title page
+# # variables are the title of pdf, watermark
+# latex_elements = {"preamble": latex.generate_preamble(html_title)}

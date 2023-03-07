35e57bddecaae1b203b63e432e3dfcc2ffcfac2c
wye <wenfeng.ye@ansys.com>
2023-03-07 15:58:34 +0100

    fix more in 4chamber case

--- ansys/heart/writer/dynawriter.py
+++ ansys/heart/writer/dynawriter.py
@@ -1160,6 +1160,9 @@ class MechanicsDynaWriter(BaseDynaWriter):
                 "superior-vena-cava",
                 "right-inferior-pulmonary-vein",
                 "right-superior-pulmonary-vein",
+                "left-superior-pulmonary-vein",
+                "left-inferior-pulmonary-vein",
+                "inferior-vena-cava",
             ]
             if isinstance(self, ZeroPressureMechanicsDynaWriter):
                 caps_to_use.extend(

ab6d5e23635de22a2537739154bb26da543e12f2
xuanyaohu <xuanyao.hu@ansys.com>
2023-11-28 11:21:25 +0100

    test ElectrophysiologyDynaWriter

--- examples/simulator/doc_ECG_scenario.py
+++ examples/simulator/doc_ECG_scenario.py
@@ -24,13 +24,16 @@ case_file = str(
     Path(Path(__file__).resolve().parents[2], "downloads", "Strocchi2020", "01", "01.case")
 )
 download_folder = str(Path(Path(__file__).resolve().parents[2], "downloads"))
-workdir = str(
-    Path(Path(__file__).resolve().parents[2], "downloads", "Strocchi2020", "01", "Biv")
-)
-# path_to_model = str(Path(workdir, str(sys.argv[1])+"heart_model.pickle"))
-path_to_model = str(Path(workdir, "heart_model.pickle"))
 
-print('11111111111111111111111')
+workdir = Path(
+    Path(__file__).resolve().parents[2], "downloads", "Strocchi2020", "01", "BiVentricle"
+)
+
+path_to_model = os.path.join(workdir, "heart_model.pickle")
+
+if not os.path.isfile(path_to_model):
+    raise FileExistsError(f"{path_to_model} not found")
+# path_to_model = str(Path(workdir, str(sys.argv[1])+"heart_model.pickle"))
 
 if not os.path.isfile(case_file):
     path_to_downloaded_file = download_case(
@@ -40,7 +43,6 @@ if not os.path.isfile(case_file):
 
 # m_size = float(sys.argv[1])
 # print('MYYYYYYYYYYY mesh : ', mesh_size)
-print('22222222222222222222')
 
 info = models.ModelInfo(
     database="Strocchi2020",
@@ -51,18 +53,8 @@ info = models.ModelInfo(
     mesh_size=0.25,
 )
 
-print('3333333333333333333333')
-# create the working directory
-info.create_workdir()
-# clean the working directory
-info.clean_workdir(extensions_to_remove=[".stl", ".vtk", ".msh.h5"])
-# dump information to stdout
-info.dump_info()
-print('4444444444444444')
-
 # instantiate a four chamber model
-model = models.FourChamber(info)
-print('555555555555555555555')
+model = models.BiVentricle(info)
 
 # # extract the simulation mesh
 # model.extract_simulation_mesh()
@@ -79,9 +71,7 @@ lsdyna_path = r"C:\Users\xuhu\lsdyna_smp_d_winx64\Other_version\mppdyna_d_winx64
 
 if not os.path.isfile(lsdyna_path):
     raise FileExistsError(f"{lsdyna_path} not found.")
-print('666666666666666666')
 
-print('77777777777777777777')
 
 # Define electrode positions and add them to model
 electrodes = [
@@ -97,34 +87,25 @@ electrodes = [
     Point(name="LL", xyz=[157.56391664248335, -81.66615972595032, 354.17867264210076]),
 ]
 model.electrodes = electrodes
-print('8888888888888888888888:', model.electrodes)
 
-if not isinstance(model, models.FourChamber):
-    raise TypeError("Expecting a FourChamber heart model.")
+if not isinstance(model, models.BiVentricle):
+    raise TypeError("Expecting a BiVentricle heart model.")
 
 # set base working directory
 model.info.workdir = str(workdir)
-print('99999999999999999999999')
 
 write_lsdyna_files = True
 
-print('1212121212')
 
 if write_lsdyna_files:
     for writer in (
         writers.ElectrophysiologyDynaWriter(model),
-        print('5552222')
     ):
         writer.update()
-        print('writer.update() pass')
-
         writer.export_databases(
             os.path.join(writer.model.info.workdir, "ECG")
         )
-        print('export_databases pass')
 
-
-print('1313131313131')
 
 # specify LS-DYNA path
 # lsdyna_path = r"C:\temporary\1\test\ls-dyna_smp_d_Dev_103633-gcc846f4b4e_winx64_ifort190.exe"

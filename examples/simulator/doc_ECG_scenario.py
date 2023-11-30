bd8e0be887e8963eb0d6b6b215152ec59e06baed
xuanyaohu <xuanyao.hu@ansys.com>
2023-11-30 14:13:14 +0100

    Update ECG scenario generation

--- examples/simulator/doc_ECG_scenario.py
+++ examples/simulator/doc_ECG_scenario.py
@@ -23,14 +23,15 @@ case_file = str(
 )
 download_folder = str(Path(Path(__file__).resolve().parents[2], "downloads"))
 
-workdir = Path(
-    Path(__file__).resolve().parents[2], "downloads", "Strocchi2020", "01", "BiVentricle"
+workdir = str(
+    Path(Path(__file__).resolve().parents[2], "downloads", "Strocchi2020", "01", "biventricle_scenario_0.25")
 )
 
 path_to_model = os.path.join(workdir, "heart_model.pickle")
+# path_to_simulation_mesh=os.path.join(workdir, "simulation_mesh.vtk")
 
-if not os.path.isfile(path_to_model):
-    raise FileExistsError(f"{path_to_model} not found")
+# if not os.path.isfile(path_to_model):
+#     raise FileExistsError(f"{path_to_model} not found")
 
 if not os.path.isfile(case_file):
     path_to_downloaded_file = download_case(
@@ -38,77 +39,94 @@ if not os.path.isfile(case_file):
     )
     unpack_case(path_to_downloaded_file)
 
+# Initialize the model
+info = models.ModelInfo(
+    database="Strocchi2020",
+    path_to_case=case_file,
+    work_directory=workdir,
+    path_to_model=path_to_model,
+    # path_to_simulation_mesh=path_to_simulation_mesh,
+    add_blood_pool=False,
+    mesh_size=0.25,
+)
 
-if __name__ == "__main__":
-    # info = models.ModelInfo(
-    #     database="Strocchi2020",
-    #     path_to_case=case_file,
-    #     work_directory=workdir,
-    #     path_to_model=path_to_model,
-    #     add_blood_pool=False,
-    #     # mesh_size=float(sys.argv[1]),
-    #     mesh_size=float(sys.argv[1]),
-    # )
+# create the working directory
+info.create_workdir()
 
-    # model = models.BiVentricle(info)
-    model: models.BiVentricle = models.BiVentricle.load_model(path_to_model)
-    model.info.mesh_size=float(sys.argv[1])
+# clean the working directory
+info.clean_workdir(extensions_to_remove=[".stl", ".vtk", ".msh.h5"])
+# dump information to stdout
 
-    # specify LS-DYNA path (last tested working versions is DEV-104399)
-    lsdyna_path = r"C:\Users\xuhu\lsdyna_smp_d_winx64\Other_version\mppdyna_d_winx64_msmpi\ls-dyna_mpp_d_Dev_104815-gc8c2d50328_winx64_ifort190_msmpi.exe"
+info.dump_info()
 
-    if not os.path.isfile(lsdyna_path):
-        raise FileExistsError(f"{lsdyna_path} not found.")
+model = models.BiVentricle(info)
 
-    # Define electrode positions and add them to model
-    electrodes = [
-        Point(name="V1", xyz=[76.53798632905277, 167.67667039945263, 384.3139099410445]),
-        Point(name="V2", xyz=[64.97540262482013, 134.94983038904573, 330.4783062379255]),
-        Point(name="V3", xyz=[81.20629301587647, 107.06245851801455, 320.58645260857344]),
-        Point(name="V4", xyz=[85.04956217691463, 59.54502732121309, 299.2838953724169]),
-        Point(name="V5", xyz=[42.31377680589025, 27.997010728192166, 275.7620409440143]),
-        Point(name="V6", xyz=[-10.105919604515957, -7.176987485426985, 270.46379012676135]),
-        Point(name="RA", xyz=[-29.55095501940962, 317.12543912177983, 468.91891094294414]),
-        Point(name="LA", xyz=[-100.27895839242505, 135.64520460914244, 222.56688206809142]),
-        Point(name="RL", xyz=[203.38825799615842, 56.19020893502452, 538.5052677637375]),
-        Point(name="LL", xyz=[157.56391664248335, -81.66615972595032, 354.17867264210076]),
-    ]
-    model.electrodes = electrodes
+# extract the simulation mesh
+model.extract_simulation_mesh()
 
-    if not isinstance(model, models.BiVentricle):
-        raise TypeError("Expecting a BiVentricle heart model.")
+# dump the model to disk for future use
+model.dump_model(path_to_model)
+# print the resulting information
+model.print_info()
 
-    # set base working directory
-    model.info.workdir = str(workdir)
 
-    write_lsdyna_files = True
+# model: models.BiVentricle = models.BiVentricle.load_model(path_to_model)
+# model.info.mesh_size=float(sys.argv[1])
 
-    if write_lsdyna_files:
-        for writer in (
-            writers.ElectrophysiologyDynaWriter(model),
-        ):
-            writer.update()        
+# # specify LS-DYNA path (last tested working versions is DEV-104399)
+# lsdyna_path = r"C:\Users\xuhu\lsdyna_smp_d_winx64\Other_version\mppdyna_d_winx64_msmpi\ls-dyna_mpp_d_Dev_104815-gc8c2d50328_winx64_ifort190_msmpi.exe"
 
-            writer.export_databases(
-                os.path.join(writer.model.info.workdir, "ECG", "ECG"+sys.argv[1])
-                # os.path.join(writer.model.info.workdir, "ECG", "ECG0.25")
-            )
+# if not os.path.isfile(lsdyna_path):
+#     raise FileExistsError(f"{lsdyna_path} not found.")
 
-    dyna_settings = DynaSettings(
-        lsdyna_path=lsdyna_path,
-        dynatype="smp",
-        num_cpus=1,
-    )
+# # Define electrode positions and add them to model
+# electrodes = [
+#     Point(name="V1", xyz=[76.53798632905277, 167.67667039945263, 384.3139099410445]),
+#     Point(name="V2", xyz=[64.97540262482013, 134.94983038904573, 330.4783062379255]),
+#     Point(name="V3", xyz=[81.20629301587647, 107.06245851801455, 320.58645260857344]),
+#     Point(name="V4", xyz=[85.04956217691463, 59.54502732121309, 299.2838953724169]),
+#     Point(name="V5", xyz=[42.31377680589025, 27.997010728192166, 275.7620409440143]),
+#     Point(name="V6", xyz=[-10.105919604515957, -7.176987485426985, 270.46379012676135]),
+#     Point(name="RA", xyz=[-29.55095501940962, 317.12543912177983, 468.91891094294414]),
+#     Point(name="LA", xyz=[-100.27895839242505, 135.64520460914244, 222.56688206809142]),
+#     Point(name="RL", xyz=[203.38825799615842, 56.19020893502452, 538.5052677637375]),
+#     Point(name="LL", xyz=[157.56391664248335, -81.66615972595032, 354.17867264210076]),
+# ]
+# model.electrodes = electrodes
 
-    run_lsdyna(
-        path_to_input = os.path.join(
-            writer.model.info.workdir, 
-            "ECG", 
-            "ECG"+sys.argv[1],
-            "main.k"
-        ),
-        settings = dyna_settings,
-        simulation_directory = os.path.join(workdir, "ECG", "ECG"+sys.argv[1]),
-    )
+# if not isinstance(model, models.BiVentricle):
+#     raise TypeError("Expecting a BiVentricle heart model.")
+
+# # set base working directory
+# model.info.workdir = str(workdir)
+
+# write_lsdyna_files = True
+
+# if write_lsdyna_files:
+#     for writer in (
+#         writers.ElectrophysiologyDynaWriter(model),
+#     ):
+#         writer.update()        
+
+#         writer.export_databases(
+#             os.path.join(writer.model.info.workdir, "ECG")
+#             # os.path.join(writer.model.info.workdir, "ECG", "ECG0.25")
+#         )
+
+# dyna_settings = DynaSettings(
+#     lsdyna_path=lsdyna_path,
+#     dynatype="smp",
+#     num_cpus=1,
+# )
+
+# run_lsdyna(
+#     path_to_input = os.path.join(
+#         writer.model.info.workdir, 
+#         "ECG", 
+#         "main.k"
+#     ),
+#     settings = dyna_settings,
+#     simulation_directory = os.path.join(workdir, "ECG"),
+# )
 
 # C:\Users\xuhu\pyheart-lib\examples\simulator\doc_ECG_scenario.py
---
+++ examples/simulator/run_ECG_scenario.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+"""
+
+(env38) PS C:\Users\xuhu\pyheart-lib\examples\simulator> python doc_ECG_scenario.py 0.25
+
+GIT BASH: 
+xuhu@LYOTRAINEE14 MINGW64 ~/pyheart-lib/examples/simulator (438-AI-modeling-for-ECG)
+$ bash ./run_ECG_scenario.sh
+"""
+params=(2 1.5 1 0.5 0.25)
+
+for param in "${params[@]}"
+do
+    python doc_ECG_scenario.py "$param"
+done

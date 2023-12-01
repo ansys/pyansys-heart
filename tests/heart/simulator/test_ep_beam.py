eb52e53521c7a5da7f5fb6108793c5983898525c
wenfengye <97118772+wenfengye@users.noreply.github.com>
2023-12-01 13:54:00 +0100

    Ep meca writer (#423)
    * draft em-meca coupling
    
    * enable Purkinje in coupling
    
    * epmeca simulator
    
    * minor
    
    * minor comments and todo
    
    * fix cap ids issue
    
    * add tie duplicated nodes
    
    * assign ssid for *EM_EP_PURKINJE_NETWORK2
    
    * fix: assign bifurcation edge to His bundle
    
    * fix: tied duplicated nodes
    
    * change His bundle param
    
    * fix: Purkinje beam ID
    
    * active atrium when coupling
    
    * minor
    
    * adapt change for dynasetting
    
    * minor
    
    * archive tie duplicated node method
    
    * add isolation part
    
    * define mat for isolation part
    
    * full heart heritate from fourchamber model
    
    * fix: in *mat295 ca2ion50
    
    * fix: in *mat295 ca2ion50 from dynalib
    
    * fix post merge error
    
    * add zerop in ep-meca
    
    * refact: EP stimulation
    
    * fix: define apex node set
    
    * compute purkinjeon guessed ED geometry
    
    * refact cell model
    
    * fix: stim node set
    
    * minor fix
    
    * fix: His bundle start end point
    
    * change full heart herirate from 4C
    
    * clean
    
    * test: fullheart type
    
    * fix: full heart init
    
    * fix2: stim node set
    
    * fix: EmControlCoupling
    
    * fix: handle His bundle speciality
    
    * write EP beam related together
    
    * fix: ep beam element ID
    
    * fix: EmControlCoupling
    
    * clean
    
    * add ep stimulation method
    
    * dummy
    
    * dummy
    
    * create isolation part for 4C model
    
    * assign purkinje name at creation
    
    * clean
    
    * refact beam net
    
    * refact conduction system
    
    * clean
    
    * set purkinje origin as apex
    
    * set beam net origin
    
    * add 2nd stimulation for 4C
    
    * fix: conduction system shared beam node
    
    * define isolation part in simulator
    
    * Revert "define isolation part in simulator"
    
    This reverts commit 9dc35dcbc6c4b971590d5b8e4dd0faf59281b0d2.
    
    * create isolation at zerop
    
    * compute conduction system after zerop
    
    * fix
    
    * change: not merge origin of purkinje to apex
    
    * ensure bundle point inside of septum
    
    * clean
    
    * change FH model zerop BC
    
    * stimule more nodes
    
    * change beam conductivity
    
    * bug fix
    
    * 4C heart zeop BC
    
    * FH zeropBC: fix LAA avoid rotation
    
    * add a contraint on apex region
    
    * fix bug
    
    * test commit
    
    * fix plot_purkinje
    
    * update unit-tests

--- tests/heart/simulator/test_ep_beam.py
+++ tests/heart/simulator/test_ep_beam.py
@@ -2,13 +2,12 @@
 import os
 import pathlib
 
-from ansys.heart.preprocessor.mesh.objects import BeamMesh, Mesh, Point
+from ansys.heart.preprocessor.mesh.objects import BeamMesh, Point
 from ansys.heart.preprocessor.models import FourChamber
 import numpy as np
 import pytest
-from pyvista import examples
 
-from tests.heart.conftest import get_workdir
+from tests.heart.conftest import get_assets_folder
 
 model: FourChamber
 model_dir: pathlib.Path
@@ -19,81 +18,57 @@ pytestmark = pytest.mark.local
 @pytest.fixture(autouse=True, scope="module")
 def get_data():
     global model, model_dir
-    # todo: file larger than 100 Mb cannot be added to package
-    model_dir = pathlib.Path(r"D:\PyAnsys-Heart\test_case\test_4c\heart_model.pickle")
+
+    # TODO: file larger than 100 Mb cannot be added to package
+
+    pickle_file = os.path.join(
+        get_assets_folder(),
+        "reference_models",
+        "strocchi2020",
+        "01",
+        "FullHeart2",
+        "heart_model.pickle",
+    )
+    model_dir = pathlib.Path(pickle_file)
+
     model = FourChamber.load_model(model_dir)
 
 
 @pytest.mark.xfail(reason="Test uses local data.")
-def test_read_purkinje_from_kfile_001():
+def test_add_beam_net():
     """Test reading Purkinje from .k files."""
 
-    # prepare example (cast directly to Mesh object)
-    grid: Mesh = Mesh(examples.load_tetbeam())
+    node_b = np.array([[0, 0, 0], [10, 10, 10]])
 
-    # assume beams are using already existed nodes except the last one
-    offset = grid.nodes.shape[0] + 1
-    beams = np.array(
-        [[3, 12], [12, 24], [24, 33], [12, 18], [12, 21], [21, 30], [3, offset]], dtype=int
-    )
-
-    # add additional point to mesh
-    extra_nodes = [[0.5, 0.5, -1], [0.5, 0.5, -0.5]]
-    # grid.nodes = np.vstack([grid.points, extra_nodes])
-
-    # mock .k file
-    keyword = "*KEYWORD\n"
-    keyword += "*NODE +\n"
-    keyword += "{:>20d}{:>20f}{:>20f}{:>20f}\n".format(offset, *extra_nodes[0])
-    keyword += "{:>20d}{:>20f}{:>20f}{:>20f}\n".format(offset + 1, *extra_nodes[1])
-    keyword += "*ELEMENT_BEAM\n"
-    # keyword += "$#   eid     pid      n1      n2\n"
-    elid = 1
-    for beam in beams:
-        keyword = keyword + "{:>8d}{:>8d}{:>8d}{:>8d}\n".format(elid, 4, *beam + 1)
-        elid += 1
-    keyword += "*NODE\n"
-    keyword += "*END\n"
-
-    # write to disc
-    temp_k_file = os.path.join(get_workdir(), "test_beams.k")
-    with open(temp_k_file, "w") as f:
-        f.write(keyword)
-
-    grid.add_purkinje_from_kfile(temp_k_file)
+    edges = np.array([[0, 0], [0, 1]])
+    mask = np.array([[False, True], [True, True]])  # first node is form solid mesh
+    model.add_beam_net(beam_nodes=node_b, edges=edges.copy(), mask=mask, pid=0, name="test")
 
     # construct mesh to compare against
-    beam_mesh = BeamMesh()
-    beam_mesh.pid = 4
-    beam_mesh.id = 1
-    beam_mesh.nodes = grid.nodes
-    beam_mesh.edges = beams
-    assert grid.beam_network[0] == beam_mesh
+    beam_mesh = BeamMesh(
+        nodes=np.vstack((model.mesh.nodes, BeamMesh.all_beam_nodes)),
+        edges=np.array([[0, 69754], [69754, 69755]]),
+        beam_nodes_mask=mask,
+    )
+    beam_mesh.pid = 0
+    beam_mesh.name = "test"
 
-    # make sure IDs are right for multi calls
-    grid.add_purkinje_from_kfile(temp_k_file)
-
-    beam_mesh2 = BeamMesh()
-    beam_mesh2.pid = 5
-    beam_mesh2.id = 2
-    beam_mesh2.nodes = grid.nodes
-    beams[-1, -1] += 2
-    beam_mesh2.edges = beams
-
-    assert grid.beam_network[1] == beam_mesh2
+    assert model.beam_network[0] == beam_mesh
 
 
 @pytest.mark.xfail(reason="Test uses local data.")
 def test_compute_SA_node():
     p = model.compute_SA_node()
-    target = Point(name="SA_node", xyz=[-49.53661854, 108.12227932, 422.97088272], node_id=19705)
+    target = Point(name="SA_node", xyz=[-48.95559814, 108.23159848, 422.91220412], node_id=22056)
     assert p.node_id == target.node_id
 
 
 @pytest.mark.xfail(reason="Test uses local data.")
 def test_compute_AV_node():
     p = model.compute_AV_node()
-    target = Point(name="AV_node", xyz=[-8.22263189, 106.95353898, 373.34239855], node_id=26409)
+    target = Point(
+        name="AV_node", xyz=np.array([-8.20742556, 106.99512699, 373.32172823]), node_id=28424
+    )
     assert p.node_id == target.node_id
 
 
@@ -101,44 +76,24 @@ def test_compute_AV_node():
 def test_compute_av_conduction():
     beam = model.compute_av_conduction()
 
-    assert np.all(beam.edges[0] == [19705, 66354])
-    assert np.all(beam.edges[-1] == [66388, 66389])
+    assert np.all(beam.edges[0] == np.array([22056, 69754]))
+    assert np.all(beam.edges[-1] == np.array([69787, 69788]))
 
 
 @pytest.mark.xfail(reason="Test uses local data.")
 def test_compute_His_conduction():
-    # need a fresh model to make sure tests are independent
-    fresh_model: FourChamber = FourChamber.load_model(model_dir)
-    fresh_model.compute_AV_node()
+    model.compute_AV_node()
+    model.compute_av_conduction()
+    beam, _ = model.compute_His_conduction()
 
-    beam = fresh_model.compute_His_conduction()
-
-    assert np.all(beam.edges[0] == [26409, 66354])
-    assert np.all(beam.edges[-1] == [66357, 66358])
+    assert np.all(beam.edges[0] == [69788, 69789])
+    assert np.all(beam.edges[-1] == [69793, 69795])
 
 
 @pytest.mark.xfail(reason="Test uses local data.")
 def test__define_hisbundle_start_end_point():
-    fresh_model: FourChamber = FourChamber.load_model(model_dir)
-    fresh_model._define_hisbundle_start_end_point(beam_length=0.8, beam_number=4)
+    model.compute_AV_node()
+    start_p, bifurcation_p = model._define_hisbundle_start_bifurcation()
 
-    a = fresh_model.septum.get_point("His septum start").node_id
-    b = fresh_model.septum.get_point("His septum end").node_id
-
-    assert a == 66354
-    assert b == 66358
-
-
-@pytest.mark.xfail(reason="Test uses local data.")
-def test_compute_bundle_branches():
-    fresh_model: FourChamber = FourChamber.load_model(model_dir)
-    # need His septum end Point
-    fresh_model._define_hisbundle_start_end_point(beam_length=0.8, beam_number=4)
-    left, right = fresh_model.compute_bundle_branches()
-    assert len(left.edges) == 45
-    assert np.all(left.edges[0] == [66358, 66354])
-    assert np.all(left.edges[-1] == [[66397, 7285]])
-
-    assert len(right.edges) == 45
-    assert np.all(right.edges[0] == [66358, 66398])
-    assert np.all(right.edges[-1] == [[66441, 25785]])
+    assert np.allclose(start_p, [2.96411229, 107.68694446, 367.01330368])
+    assert np.allclose(bifurcation_p, [7.24693771, 106.92654286, 365.52226939])

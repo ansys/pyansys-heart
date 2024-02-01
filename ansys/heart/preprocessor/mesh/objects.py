a2917423495b8fa796aac2b4db40692cf019df34
M. Hoeijmakers <martijn.hoeijmakers@ansys.com>
2024-02-01 15:55:30 +0100

    Fix: add alternative tesslation method

--- ansys/heart/preprocessor/mesh/objects.py
+++ ansys/heart/preprocessor/mesh/objects.py
@@ -416,6 +416,42 @@ class Cap(Feature):
         """Type."""
         return
 
+    def tessellate1(self, use_centroid: bool = False) -> np.ndarray:
+        """Tessellate the cap.
+
+        Parameters
+        ----------
+        use_centroid : bool, optional
+            Use a central node for tessellation, by default False
+
+        Returns
+        -------
+        np.ndarray
+            Triangles forming the tessellation.
+        """
+        if not use_centroid:
+            tris = []
+            for ii, _ in enumerate(self.node_ids[0:-2]):
+                # first node is reference node
+                tri = [self.node_ids[0], self.node_ids[ii + 1], self.node_ids[ii + 2]]
+                tris.append(tri)
+            self.triangles = np.array(tris, dtype=int)
+        elif use_centroid:
+            if not self.centroid_id:
+                LOGGER.error("Not able to create the tessellation with the cap center.")
+                return None
+
+            ref_node = self.centroid_id
+            num_triangles = self.node_ids.shape[0] + 1
+            tris = [[ref_node, self.node_ids[0], self.node_ids[1]]]
+            for ii, _ in enumerate(self.node_ids[0:-2]):
+                tri = [ref_node, self.node_ids[ii + 1], self.node_ids[ii + 2]]
+                tris.append(tri)
+            tris.append([ref_node, self.node_ids[-1], self.node_ids[0]])
+            self.triangles = np.array(tris, dtype=int)
+
+        return self.triangles
+
     def tessellate(self, center_point_id=None) -> np.ndarray:
         """
         Form triangles with the node ids.

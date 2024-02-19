48c283c195722e9d3524128759ae40370b849692
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-02-19 09:34:14 +0100

    fix: add the additional files

--- src/ansys/heart/__init__.py
+++ src/ansys/heart/__init__.py
@@ -1,7 +1,17 @@
 """Collection of modules for heart modeling."""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata
+
+# Read from the pyproject.toml
+# major, minor, patch
+
 import logging
 
 from ansys.heart.core.logging import Logger
 
+__version__ = importlib_metadata.version("pyansys-heart")
+
 LOG = Logger(level=logging.DEBUG, to_file=False, to_stdout=True)
-LOG.debug("Loaded logging module as LOG")

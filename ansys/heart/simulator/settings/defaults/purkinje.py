f3f7697f5e95336f93f6fcf5fe2e8c35826b4e05
kelhouar <karim.elhouari@ansys.com>
2024-02-23 17:25:41 +0100

    correct params Fixes #451

--- ansys/heart/simulator/settings/defaults/purkinje.py
+++ ansys/heart/simulator/settings/defaults/purkinje.py
@@ -3,9 +3,9 @@ from pint import Quantity
 
 """Construction parameters."""
 build = {
-    "edgelen": Quantity(2, "mm"),
-    "ngen": Quantity(50, "dimensionless"),
-    "nbrinit": Quantity(8, "dimensionless"),
+    "edgelen": Quantity(0.5, "mm"),
+    "ngen": Quantity(200, "dimensionless"),
+    "nbrinit": Quantity(3, "dimensionless"),
     "nsplit": Quantity(2, "dimensionless"),
     "pmjtype": Quantity(1, "dimensionless"),
     "pmjradius": Quantity(0.7, "dimensionless"),

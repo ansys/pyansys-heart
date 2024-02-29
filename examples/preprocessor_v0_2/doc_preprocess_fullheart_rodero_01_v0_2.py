5ed6a8951f83b495b60256b500250397c3a785c4
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-02-29 09:38:36 +0100

    fix: update the code style

--- examples/preprocessor_v0_2/doc_preprocess_fullheart_rodero_01_v0_2.py
+++ examples/preprocessor_v0_2/doc_preprocess_fullheart_rodero_01_v0_2.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """
 
 Create a full-heart model
@@ -5,6 +27,7 @@ Create a full-heart model
 This example shows you how to download a case from the Rodero et al (2020) database
 and process that case into a simulation-ready four chamber heart model.
 """
+
 import os
 
 os.environ["ANSYS_HEART_MODEL_VERSION"] = "v0.2"

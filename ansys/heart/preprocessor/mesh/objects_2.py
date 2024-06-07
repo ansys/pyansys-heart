26f1f54a589a2e01fdc6999298eb4b35f34de752
mhoeijm <martijn.hoeijmakers@ansys.com>
2024-06-07 16:13:26 +0200

    feat: rework Mesh object

---
+++ ansys/heart/preprocessor/mesh/objects_2.py
@@ -0,0 +1,229 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Module that contains classes relevant for the mesh.
+
+Such as a Mesh object, Part object, Features, etc.
+
+"""
+
+import copy  # noqa
+import pathlib
+from typing import List, Optional, Tuple, Union
+
+from ansys.heart.core import LOG as LOGGER
+import ansys.heart.preprocessor.mesh.connectivity as connect
+import ansys.heart.preprocessor.mesh.geodisc as geodisc
+import ansys.heart.preprocessor.mesh.vtkmethods as vtkmethods
+from ansys.heart.simulator.settings.material.material import MechanicalMaterialModel
+import numpy as np
+
+try:
+    import pyvista as pv
+except ImportError:
+    LOGGER.warning("Importing pyvista failed. Install with: pip install pyvista")
+
+
+def _get_fill_data(
+    mesh1: pv.UnstructuredGrid | pv.PolyData,
+    mesh2: pv.UnstructuredGrid | pv.PolyData,
+    array_name: str,
+    array_association: str = "cell",
+    pad_value_int: int = None,
+    pad_value_float: float = None,
+):
+    if array_name not in mesh1.array_names:
+        return
+
+    if array_association == "cell":
+        if array_name in mesh2.cell_data.keys():
+            return mesh2.cell_data[array_name]
+
+        array = mesh1.cell_data[array_name]
+        n_pads = mesh2.n_cells
+
+    elif array_association == "point":
+        if array_name in mesh2.point_data.keys():
+            return mesh2.point_data[array_name]
+
+        array = mesh1.point_data[array_name]
+        n_pads = mesh2.n_points
+
+    shape = list(array.shape)
+    shape[0] = n_pads
+    shape = tuple(shape)
+
+    pad_array = np.zeros(shape, dtype=array.dtype)
+
+    if isinstance(array[0], (np.float64, np.float32)):
+        if not pad_value_float:
+            pad_array = pad_array * np.nan
+        else:
+            pad_array = pad_array * pad_value_float
+
+    elif isinstance(array[0], (np.int32, np.int64)):
+        if pad_value_int:
+            pad_array = pad_array + pad_value_int
+
+    return pad_array
+
+
+class Mesh(pv.UnstructuredGrid):
+    """Mesh class: inherits from pyvista UnstructuredGrid.
+
+    Notes
+    -----
+    Only tetrahedrons are supported.
+    Additional attributes are added on top of the pyvista UnstructuredGrid class
+    """
+
+    @property
+    def nodes(self):
+        """Node coordinates."""
+        return np.array(self.points)
+
+    @nodes.setter
+    def nodes(self, array: np.ndarray):
+        if isinstance(array, type(None)):
+            return
+        try:
+            num_extra_points = array.shape[0] - self.points.shape[0]
+            self.points = array
+            if num_extra_points > 0:
+                for key in self.point_data.keys():
+                    shape = self.point_data[key].shape
+                    dtype = self.point_data[key].dtype
+
+                    # vectors
+                    if len(shape) > 1:
+                        append_shape = (num_extra_points, shape[1])
+                        self.point_data[key] = np.vstack(
+                            [self.point_data[key], np.empty(append_shape, dtype) * np.nan]
+                        )
+                    # scalars
+                    else:
+                        append_shape = (num_extra_points,)
+                        self.point_data[key] = np.append(
+                            self.point_data[key], np.empty(append_shape, dtype) * np.nan
+                        )
+
+            elif num_extra_points < 0:
+                raise NotImplementedError(
+                    "Assigning less nodes than the original not implemented yet."
+                )
+
+        except:
+            LOGGER.warning("Failed to set nodes.")
+            return
+
+    @property
+    def tetrahedrons(self):
+        """Tetrahedrons num_tetra x 4."""
+        return self.cells_dict[pv.CellType.TETRA]
+
+    @property
+    def triangles(self):
+        """ "Get all triangles of the mesh."""
+        return self.cells_dict[pv.CellType.TRIANGLE]
+
+    @property
+    def lines(self):
+        """ "Get all triangles of the mesh."""
+        return self.cells_dict[pv.CellType.LINE]
+
+    @tetrahedrons.setter
+    def tetrahedrons(self, value: np.ndarray):
+        # sets tetrahedrons of UnstructuredGrid
+        try:
+            points = self.points
+            celltypes = np.full(value.shape[0], pv.CellType.TETRA, dtype=np.int8)
+            tetra = np.hstack([np.full(len(celltypes), 4)[:, None], value])
+            super().__init__(tetra, celltypes, points)
+        except:
+            LOGGER.warning("Failed to set tetrahedrons.")
+            return
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # self.triangles: np.ndarray = None
+        """Faces that make up the tetrahedrons."""
+        self.face_types: np.ndarray = None
+        """Type of face: 1: interior face, 2: boundary face, 3: interface face."""
+        self.conn = {"c0": [], "c1": []}
+        """Face-tetra connectivity array."""
+        # TODO: just store used nodes in interfaces and boundaries
+        # and add mapper to map from local to global (volume mesh) node ids
+        # self.interfaces: List[SurfaceMesh] = []
+        """List of surface meshes that make up the interface between different parts."""
+        # self.boundaries: List[SurfaceMesh] = []
+        """List of boundary surface meshes within the part."""
+        pass
+
+    def add_mesh(
+        self,
+        mesh: pv.PolyData | pv.UnstructuredGrid,
+        keep_data: bool = True,
+        fill_float: np.float64 = np.nan,
+        fill_int: int = -1,
+    ):
+        """Add another mesh to this object.
+
+        Parameters
+        ----------
+        mesh : pv.PolyData | pv.UnstructuredGrid
+            Mesh to add, either PolyData or UnstructuredGrid
+        keep_data : bool, optional
+            Flag specifying whether to try to keep mesh point/cell data, by default True
+        """
+        filldata = 0
+
+        cell_arrays = self.cell_data.keys()
+        fill_array = _get_fill_data(self, mesh, cell_arrays[0], "cell")
+
+        if keep_data:
+            # add cell/point arrays in self
+            cell_data_names = [k for k in mesh.cell_data.keys()]
+            point_data_names = [k for k in mesh.point_data.keys()]
+
+            for name in cell_data_names:
+                self.cell_data[name] = _get_fill_data(
+                    mesh, self, name, "cell", fill_int, fill_float
+                )
+
+            for name in point_data_names:
+                self.point_data[name] = _get_fill_data(
+                    mesh, self, name, "point", fill_int, fill_float
+                )
+
+            # add cell/point arrays mesh to be added
+            cell_data_names = [k for k in self.cell_data.keys()]
+            point_data_names = [k for k in self.point_data.keys()]
+
+            for name in cell_data_names:
+                mesh.cell_data[name] = _get_fill_data(self, mesh, name, "cell")
+
+            for name in point_data_names:
+                mesh.point_data[name] = _get_fill_data(self, mesh, name, "point")
+
+        return self + mesh
--- tests/heart/preprocessor/test_mesh_objects_revised.py
+++ tests/heart/preprocessor/test_mesh_objects_revised.py
@@ -22,7 +22,7 @@
 
 import os
 
-from ansys.heart.preprocessor.mesh.objects import Mesh, SurfaceMesh
+from ansys.heart.preprocessor.mesh.objects_2 import Mesh
 import numpy as np
 import pytest  # noqa F401
 import pyvista as pv  # noqa F401
@@ -49,6 +49,38 @@ def test_mesh_object_assign():
 
     grid = pv.UnstructuredGrid(cells, cell_types, points)
 
+    grid1 = Mesh(cells, cell_types, points)
+
     mask = grid.celltypes == pv.CellType.LINE
     lines = grid.cells_dict[pv.CellType.LINE]
     lines = grid.extract_cells(mask)
+
+    grid1.cell_data["cdata_tet1"] = 1
+    grid1.point_data["pdata_tet1"] = 2
+
+    # TODO
+    # add lines
+    lines = pv.lines_from_points([points[0, :], [-1, 0, 0], [-2, 0, 0]])
+    # create some dummy data
+    lines["cdata_lines1"] = np.array([10, 11], dtype=np.int32)
+    lines["cdata_lines2"] = np.array([10.5, 11.5], dtype=np.float64)
+    lines["pdata_lines1"] = np.array([[100, 101], [102, 103], [104, 105]], dtype=np.float64)
+
+    grid2 = grid1.add_mesh(lines)
+
+    # check array names
+    for name in ["cdata_lines1", "cdata_lines2", "cdata_tet1", "pdata_tet1", "pdata_lines1"]:
+        assert name in grid2.array_names, f"Array with name {name} does  not exist"
+
+    # check data types
+    assert isinstance(grid2.cell_data["cdata_lines1"][0], np.int32)
+    assert isinstance(grid2.cell_data["cdata_lines2"][0], np.float64)
+
+    # check shape of array
+    assert grid2["pdata_lines1"].shape[1] == lines["pdata_lines1"].shape[1]
+
+    grid
+
+    assert grid.__add__(lines) == (grid + lines)
+
+    pass

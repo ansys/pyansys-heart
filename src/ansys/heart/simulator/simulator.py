0275deff2cc14e72a6a8ac6b907161bede7e0b98
mhoeijm <martijn.hoeijmakers@ansys.com>
2024-10-02 15:06:23 +0200

    maint: move element_orth

--- src/ansys/heart/simulator/simulator.py
+++ src/ansys/heart/simulator/simulator.py
@@ -43,7 +43,7 @@ import numpy as np
 import pyvista as pv
 
 from ansys.heart.core import LOG as LOGGER
-from ansys.heart.misc.element_orth import _read_orth_element_kfile
+from ansys.heart.core.helpers.element_orth import _read_orth_element_kfile
 from ansys.heart.postprocessor.auto_process import mech_post, zerop_post
 from ansys.heart.postprocessor.laplace_post import (
     compute_la_fiber_cs,

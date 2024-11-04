4d0f04d7098296e50e20fe358a89b0539f552631
mhoeijm <martijn.hoeijmakers@ansys.com>
2024-11-04 15:51:24 +0100

    maint: style

--- tests/heart/core/test_heart_model_plot.py
+++ tests/heart/core/test_heart_model_plot.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2023 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import os
 
 if os.getenv("GITHUB_ACTIONS"):
@@ -20,7 +42,7 @@ def _mock_biventricle():
     """Mock biventricular model."""
     mesh = pv.examples.load_tetbeam()
     mesh.points = mesh.points*1e1
-    mesh.cell_data["part-id"] = 1    
+    mesh.cell_data["part-id"] = 1
     fibers = np.repeat([1.0, 0.0, 0.0], mesh.n_cells, axis = 0).reshape((3, mesh.n_cells)).T
     mesh.cell_data["fiber"] = fibers
     mock_biventricle: models.BiVentricle = models.BiVentricle(mock.Mock(models.ModelInfo))
@@ -31,7 +53,7 @@ def test_heart_model_plot_mesh(_mock_biventricle):
     """Test plotting the mesh."""
     # mock .show or pv.OFF_SCREEN?
     _mock_biventricle.plot_mesh()
-    
+
 
 def test_heart_model_plot_part(_mock_biventricle):
     """Test plotting a part."""

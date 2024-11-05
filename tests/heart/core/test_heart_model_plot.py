2392f72994717adf0439bec4dd7ed1e1dd2de1c1
mhoeijm <martijn.hoeijmakers@ansys.com>
2024-11-05 09:33:54 +0100

    maint: adopt mock for plotter.show

--- tests/heart/core/test_heart_model_plot.py
+++ tests/heart/core/test_heart_model_plot.py
@@ -34,11 +34,9 @@ import unittest.mock as mock
 import ansys.heart.core.models as models
 import pyvista as pv
 from ansys.heart.core.objects import Part
-pv.OFF_SCREEN = True
-
 
 @pytest.fixture
-def _mock_biventricle():
+def _mock_input(mocker):
     """Mock biventricular model."""
     mesh = pv.examples.load_tetbeam()
     mesh.points = mesh.points*1e1
@@ -47,22 +45,28 @@ def _mock_biventricle():
     mesh.cell_data["fiber"] = fibers
     mock_biventricle: models.BiVentricle = models.BiVentricle(mock.Mock(models.ModelInfo))
     mock_biventricle.mesh = mesh
-    return mock_biventricle
+    mock_show = mocker.patch("pyvista.Plotter.show")
+    return mock_biventricle, mock_show
 
-def test_heart_model_plot_mesh(_mock_biventricle):
+def test_heart_model_plot_mesh(_mock_input):
     """Test plotting the mesh."""
-    # mock .show or pv.OFF_SCREEN?
-    _mock_biventricle.plot_mesh()
+    mock_biventricle, mock_show = _mock_input
+    mock_biventricle.plot_mesh()
+    mock_show.assert_called_once()
 
 
-def test_heart_model_plot_part(_mock_biventricle):
+def test_heart_model_plot_part(_mock_input):
     """Test plotting a part."""
+    mock_biventricle, mock_show = _mock_input
     mock_part = mock.Mock(Part)
     mock_part.element_ids = np.array([0, 1, 2, 3, 4])
-    _mock_biventricle.plot_part(mock_part)
+    mock_biventricle.plot_part(mock_part)
+    mock_show.assert_called_once()
 
-def test_heart_model_plot_fibers(_mock_biventricle):
+def test_heart_model_plot_fibers(_mock_input):
     """Test plotting of fibers."""
-    assert isinstance(_mock_biventricle.plot_fibers(), pv.Plotter)
-    _mock_biventricle.mesh.points = _mock_biventricle.mesh.points/1e1
-    assert not _mock_biventricle.plot_fibers()
+    mock_biventricle, mock_show = _mock_input
+    assert isinstance(mock_biventricle.plot_fibers(), pv.Plotter)
+    mock_biventricle.mesh.points = mock_biventricle.mesh.points/1e1
+    assert not mock_biventricle.plot_fibers()
+    mock_show.assert_called_once()

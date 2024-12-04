49a4b65869c91fd1c47f5efb7a2ff438e0cb3769
mhoeijm <martijn.hoeijmakers@ansys.com>
2024-12-04 09:52:04 +0100

    feat: allow user to set number of cpus used for meshing

--- src/ansys/heart/preprocessor/mesher.py
+++ src/ansys/heart/preprocessor/mesher.py
@@ -43,6 +43,7 @@ from ansys.heart.preprocessor.input import _InputBoundary, _InputModel
 _supported_fluent_versions = ["24.2", "24.1"]
 _show_fluent_gui: bool = True
 _uses_container: bool = True
+_num_cpus: bool = 2
 
 try:
     _show_fluent_gui = bool(int(os.environ["SHOW_FLUENT_GUI"]))

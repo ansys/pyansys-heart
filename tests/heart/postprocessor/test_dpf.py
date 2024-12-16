ddd6c3e8ace94f41e2959188bff53c1a4446d7f2
wenfengye <wenfeng.ye@ansys.com>
2024-12-16 17:29:00 +0100

    minor

--- tests/heart/postprocessor/test_dpf.py
+++ tests/heart/postprocessor/test_dpf.py
@@ -63,7 +63,7 @@ def test_d3plot_reader():
     assert isinstance(d3plot.get_ep_fields(), dpf.FieldsContainer)
 
 
-@pytest.xfail(reason="This function fails on Github but not local.")
+@pytest.mark.xfail(reason="This function fails on Github but not local.")
 def test_d3plot_reader2():
     fn = os.path.join(get_assets_folder(), "post", "main", "d3plot")
     d3plot = D3plotReader(fn)

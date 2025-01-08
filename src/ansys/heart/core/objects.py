ff04c7e33268b174e44a2376bff775aee487cced
mhoeijm <martijn.hoeijmakers@ansys.com>
2025-01-08 10:47:14 +0100

    maint: remove deprecated node setter

--- src/ansys/heart/core/objects.py
+++ src/ansys/heart/core/objects.py
@@ -490,44 +490,6 @@ class Mesh(pv.UnstructuredGrid):
         """Node coordinates."""
         return np.array(self.points)
 
-    @nodes.setter
-    @deprecated(
-        reason="""Setting nodes through this property is deprecated.
-        Use add_surface, add_volume or .points instead"""
-    )
-    def nodes(self, array: np.ndarray):
-        if isinstance(array, type(None)):
-            return
-        try:
-            num_extra_points = array.shape[0] - self.points.shape[0]
-            self.points = array
-            if num_extra_points > 0:
-                for key in self.point_data.keys():
-                    shape = self.point_data[key].shape
-                    dtype = self.point_data[key].dtype
-
-                    # vectors
-                    if len(shape) > 1:
-                        append_shape = (num_extra_points, shape[1])
-                        self.point_data[key] = np.vstack(
-                            [self.point_data[key], np.empty(append_shape, dtype) * np.nan]
-                        )
-                    # scalars
-                    else:
-                        append_shape = (num_extra_points,)
-                        self.point_data[key] = np.append(
-                            self.point_data[key], np.empty(append_shape, dtype) * np.nan
-                        )
-
-            elif num_extra_points < 0:
-                raise NotImplementedError(
-                    "Assigning less nodes than the original not implemented yet."
-                )
-
-        except Exception:
-            LOGGER.warning("Failed to set nodes.")
-            return
-
     @property
     def tetrahedrons(self):
         """Tetrahedrons num_tetra x 4."""

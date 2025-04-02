df98cec239b8963427f2900821dbd62be7e4a17b
mhoeijm <martijn.hoeijmakers@ansys.com>
2025-04-02 15:50:46 +0200

    Revert "fix mock"
    This reverts commit b9bf4ca7fa612527aaf0da05e30d6c6cf4af88d5.

--- tests/heart/simulator/test_simulator.py
+++ tests/heart/simulator/test_simulator.py
@@ -76,7 +76,6 @@ def mechanics_simulator() -> simulators.MechanicsSimulator:
         setting = mock.Mock(spec=DynaSettings)
         setting.lsdyna_path = ""
         simulation_directory = "."
-        setting.platform = ""
         simulator = simulators.MechanicsSimulator(model, setting, simulation_directory)
         yield simulator
 

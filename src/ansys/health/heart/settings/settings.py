cc3ad851d1011c9e7e2c95e2999efe577ce9c547
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-04-18 22:38:26 +0200

    ci: dbg intelmpi on runner

--- src/ansys/health/heart/settings/settings.py
+++ src/ansys/health/heart/settings/settings.py
@@ -1076,6 +1076,7 @@ class DynaSettings:
 
         # expand any environment variables if any
         commands = [os.path.expandvars(c) for c in commands]
+        LOGGER.info(f"LS-DYNA commands: {' '.join(commands)}")
 
         return commands
 

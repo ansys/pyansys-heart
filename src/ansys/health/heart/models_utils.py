08da40f9677f170a63677436d215e67b82ad6e19
KarimElHouari <karim.elhouari@ansys.com>
2025-04-25 16:03:10 +0200

    style

--- src/ansys/health/heart/models_utils.py
+++ src/ansys/health/heart/models_utils.py
@@ -362,7 +362,7 @@ class HeartModelUtils:
             line_length=None,
         )
         npoints = left_bundle.mesh.n_points
-        start_connect_id = int(npoints*0.4)
+        start_connect_id = int(npoints * 0.4)
         left_bundle.is_connected[start_connect_id:-1] = 1
         left_bundle.up_path = his_left
         left_bundle.down_path = left_purkinje

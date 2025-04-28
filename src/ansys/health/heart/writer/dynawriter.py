43cd39aeabe05423667031b9135f1200940c3a8a
OlivierCrabbe <olivier.crabbe@ansys.com>
2025-04-28 09:00:18 +0200

    added EM_CONTROL_TIMESTEP to fiber generation deck updated corresponding reference_model decks

--- src/ansys/health/heart/writer/dynawriter.py
+++ src/ansys/health/heart/writer/dynawriter.py
@@ -2102,6 +2102,8 @@ class FiberGenerationDynaWriter(BaseDynaWriter):
             )
         )
 
+        self.kw_database.ep_settings.append(keywords.EmControlTimestep(dtcons=1))
+        
         # use defaults
         self.kw_database.ep_settings.append(custom_keywords.EmControlEp())
 

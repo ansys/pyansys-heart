9d2cb9a14b7300013122d6d43d75c0ccae338386
wenfengye <wenfeng.ye@ansys.com>
2025-04-29 16:33:17 +0200

    refine example with doc

--- examples/simulator/doc_MECA_simulator_LeftVentricle_pr.py
+++ examples/simulator/doc_MECA_simulator_LeftVentricle_pr.py
@@ -24,10 +24,19 @@
 
 Run a left ventricle mechanical simulation
 ------------------------------------------
-This example shows how to generate a left-ventricle model and set it up for the
-mechanical simulation. It loads the surfaces, creates the mesh and computes
-the fiber orientation, assign the customized material and then simulates
-the heart contraction.
+This example shows:
+    - how to generate a left-ventricle model from labeled surface
+    - set it up for the mechanical simulation including:
+        - fiber generation
+        - material assignment
+        - boundary conditions tuning
+        - compute stress-free configuration
+        - run the simulation for one heartbeat
+    - post-process the results
+        - plot the stress-free configuration v.s. end-diastole configuration
+        - plot Klotz curve
+        - animate the simulation results
+        - plot PV loop
 """
 
 ###############################################################################
@@ -43,11 +52,14 @@ import os
 from pathlib import Path
 import time
 
+import matplotlib.pyplot as plt
+from pint import Quantity
 import pyvista as pv
 
 from ansys.health.heart.examples import get_input_leftventricle
 import ansys.health.heart.models as models
 from ansys.health.heart.post.auto_process import zerop_post
+from ansys.health.heart.post.dpf_utils import ICVoutReader
 from ansys.health.heart.settings.material.material import ACTIVE, ANISO, ISO, Mat295
 from ansys.health.heart.simulator import DynaSettings, MechanicsSimulator
 
@@ -56,10 +68,16 @@ from ansys.health.heart.simulator import DynaSettings, MechanicsSimulator
 # by setting the environment variable ``ANSYS_DPF_ACCEPT_LA`` to ``Y``.
 # for instance by: os.environ["ANSYS_DPF_ACCEPT_LA"] = "Y"
 
+###############################################################################
+# Get the model input surface and part definitions.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 path_to_surface, path_to_part_definitions = get_input_leftventricle()
+
+# Left ventricle is enclosed by 4 surfaces
 surface = pv.read(path_to_surface)
 surface.plot()
 
+# The part is defined from the JSON file.
 with open(path_to_part_definitions, "r") as f:
     part_definitions = json.load(f)
 print(part_definitions)
@@ -67,16 +85,23 @@ print(part_definitions)
 ###############################################################################
 # Load the input surface
 # ~~~~~~~~~~~~~~~~~~~~~~~
-# Set the working directory and path to the model
+# Set the working directory
 workdir = Path.home() / "pyansys-heart" / "downloads" / "Rodero2021" / "01" / "LeftVentricle"
 model: models.LeftVentricle = models.LeftVentricle(working_directory=workdir)
 
 # Load the input surface.
 model.load_input(surface, part_definitions, scalar="surface-id")
 
-# mesh the volume
+# Mesh the volume.
 model.mesh_volume(use_wrapper=True, global_mesh_size=4.0, _global_wrap_size=4.0)
+###############################################################################
+# .. note::
+#    Mesh is over coarsed for demonstration purpose. In practice, the mesh size should be smaller.
+
+# Update the model.
 model._update_parts()
+
+# Plot mesh.
 model.plot_mesh()
 
 ###############################################################################
@@ -85,7 +110,7 @@ model.plot_mesh()
 # Instantiate the simulator and define settings.
 
 # Specify the LS-DYNA path. (The last tested working version is ``intelmpi-linux-DEV-106117``.)
-# TODO: replace path
+# TODO: remove before merge
 lsdyna_path = r"D:\wsl\lsdyna_mpp\ls-dyna_mpp_d_R16"
 
 # Instantiate DYNA settings.
@@ -109,13 +134,14 @@ simulator.settings.load_defaults()
 ###############################################################################
 # Compute fiber orientation
 # ~~~~~~~~~~~~~~~~~~~~~~~~~
-# Compute fiber orientation and plot the fibers on the entire model.
-
-# Compute ventricular fibers.
+# Compute fiber orientation and plot the fibers.
 simulator.compute_fibers(method="D-RBM")
 simulator.model.plot_fibers(n_seed_points=2000)
 
 
+###############################################################################
+# Assign the material to the left ventricle.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 myocardium = Mat295(
     rho=0.001,
     iso=ISO(itype=-3, beta=2, kappa=1.0, k1=0.20e-3, k2=6.55),
@@ -125,9 +151,17 @@ myocardium = Mat295(
         k1fs=0.15e-3,
         k2fs=6.28,
     ),
-    active=ACTIVE(),  # Default active model
+    active=ACTIVE(),  # Use default active model
 )
 simulator.model.left_ventricle.meca_material = myocardium
+
+################################################################################
+# Compute the stress-free configuration
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###############################################################################
+# .. note::
+#    Input geometry is assumed to be at the end-of-diastole with a
+#    pressure is set to 15 mmHg, you can modify it in the settings.
 
 simulator.compute_stress_free_configuration()
 
@@ -144,15 +178,44 @@ plotter.add_mesh(zerop, color="red", opacity=0.3, label="zeropressure")
 plotter.add_mesh(simulator.model.mesh, color="grey", opacity=0.2, label="end-of-diastole")
 plotter.add_legend()
 plotter.show()
-###############################################################################
-# Start the main simulation
-# ~~~~~~~~~~~~~~~~~~~~~~~~~
-# Start the main meca simulation.
 
-# run 1 heartbeat
+###############################################################################
+# the main simulation
+# ~~~~~~~~~~~~~~~~~~~
+###############################################################################
+# .. note::
+#    By defaylt, the simulation is coupled to the circulation system model which has
+#    constant-preload and winkessel-type afterload
+#    The simulation is set to run for 1 heartbeat by default.
+
+# Tune the boundary conditions (springs at valves regions) which is supposed for this model.
+simulator.settings.mechanics.boundary_conditions.valve["stiffness"] = Quantity(0.02, "MPa/mm")
+
+# Start the mechanical simulation.
 simulator.simulate()
 
 
+###############################################################################
+# Post processing
+# ~~~~~~~~~~~~~~~
+# TODO: remove before merge
+workdir = r"C:\Users\wye\pyansys-heart\downloads\Rodero2021\01\LeftVentricle"
+
+# Plot Pressure volume loop
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# read LS-DYNA binout file
+icvout = ICVoutReader(os.path.join(workdir, "simulation-MECA", "main-mechanics", "binout0000"))
+pressure = icvout.get_pressure(1)
+volume = icvout.get_volume(1)
+plt.plot(volume / 1000, pressure * 7500, label="Left ventricle")
+plt.xlabel("Volume (mL)")
+plt.ylabel("Pressure (mmHg)")
+plt.title("Pressure-volume loop")
+plt.show()
+
+# Plot the displacement field
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 mesh_folder = os.path.join(workdir, "simulation-MECA", "main-mechanics", "post", "vtks")
 mesh_files = sorted(glob.glob(f"{mesh_folder}/*.vtu"))
 mesh_list = []
@@ -160,8 +223,9 @@ for mesh_file in mesh_files:
     mesh = pv.read(mesh_file)
     mesh.set_active_vectors("displacement")
     mesh_list.append(mesh)
+
 # Create a PyVista plotter
-plotter = pv.Plotter(notebook=True)
+plotter = pv.Plotter()
 
 if mesh_files:
     actor = plotter.add_mesh(mesh_list[0])
@@ -173,10 +237,10 @@ plotter.show(interactive_update=True)
 def animate():
     for m in mesh_list:
         plotter.clear()
-        plotter.add_mesh(m)
+        plotter.add_mesh(m, show_edges=True, opacity=0.5, scalars=None)
         plotter.update()
         plotter.render()
-        time.sleep(0.5)  # Control animation speed
+        time.sleep(0.1)  # Control animation speed
 
 
 animate()

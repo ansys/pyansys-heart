6465a1b65302ac882654aeebc3f9b5ae68279452
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-04-30 15:12:04 +0200

    docs: fix print on exception

--- src/ansys/health/heart/exceptions.py
+++ src/ansys/health/heart/exceptions.py
@@ -26,9 +26,9 @@
 class LSDYNATerminationError(BaseException):
     """Exception raised when ``Normal Termination`` is not found in the LS-DYNA logs."""
 
-    def __init__(self, message: str | list):
+    def __init__(self, message: str | list = ""):
         if isinstance(message, list):
-            message = "\n".join(message)
+            message = "".join(message)
         super().__init__(f"The LS-DYNA process did not terminate as expected: {message}")
 
 

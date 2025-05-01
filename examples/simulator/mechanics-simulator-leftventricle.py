1abdf0275db66d3b44dc3ca4d2a63434c1c8189d
Martijn <102799582+mhoeijm@users.noreply.github.com>
2025-05-01 17:05:50 +0200

    Update examples/simulator/mechanics-simulator-leftventricle.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/simulator/mechanics-simulator-leftventricle.py
+++ examples/simulator/mechanics-simulator-leftventricle.py
@@ -224,7 +224,7 @@ simulator.simulate()
 # .. note::
 #    By default, the simulation is coupled to an open-loop circulation model with
 #    a constant preload and a Windkessel-type afterload. Moreover, the simulation
-#    is set to run for a single heartbeat by default.
+#    is set to run for a single heartbeat.
 
 ###############################################################################
 # Postprocessing

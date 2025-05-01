ad7ca2bca907e1e7a5887f42adbac896e574577d
Martijn <102799582+mhoeijm@users.noreply.github.com>
2025-05-01 17:02:06 +0200

    Update examples/preprocessor/compute-atrial-fibers_pr.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/preprocessor/compute-atrial-fibers_pr.py
+++ examples/preprocessor/compute-atrial-fibers_pr.py
@@ -29,9 +29,9 @@ This example shows how to generate atrial fibers using the Laplace-Dirichlet Rul
 
 ###############################################################################
 # .. warning::
-#    When using a standalone version of the DPF Server you will need to accept the license terms
-#    https://dpf.docs.pyansys.com/version/stable/getting_started/licensing.html. The license terms
-#    in the license agreement can be accepted by setting the environment variable:
+#    When using a standalone version of the DPF Server, you must accept the `license terms
+#    <https://dpf.docs.pyansys.com/version/stable/getting_started/licensing.html>`_. To
+#    accept these terms, you can set this environment variable:
 #
 #    .. code-block:: python
 #

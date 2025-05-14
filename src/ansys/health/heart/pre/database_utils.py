563347d2998eb046d5c2fd8b3382702ec5efb01b
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-05-14 16:14:48 +0200

    refactor: use pyvista method

--- src/ansys/health/heart/pre/database_utils.py
+++ src/ansys/health/heart/pre/database_utils.py
@@ -31,7 +31,6 @@ import pyvista as pv
 
 from ansys.health.heart import LOG as LOGGER
 from ansys.health.heart.utils.connectivity import face_tetra_connectivity
-import ansys.health.heart.utils.misc as geodisc
 
 
 def _read_input_mesh(mesh_path: str, database: str) -> pv.UnstructuredGrid:
@@ -396,7 +395,16 @@ def _smooth_boundary_edges(
 
                 # project points
                 if project_edge_loop:
-                    new_points = geodisc.project_3d_points(edges.points)[0]
+                    # fit points through plane.
+                    _, plane_center, plane_normal = pv.fit_plane_to_points(
+                        edges.points, return_meta=True
+                    )
+                    # project points to the plane.
+                    new_points = (
+                        pv.PolyData(edges.points)
+                        .project_points_to_plane(plane_center, plane_normal)
+                        .points
+                    )
                     edges.points = new_points
 
                 sorted_points = edges.points[sorted_edges_array[:, 0], :]

b70cf6ff75d514a0d85f4a195723b2983bdc393e
Martijn <102799582+mhoeijm@users.noreply.github.com>
2025-05-16 19:03:49 +0200

    Update src/ansys/health/heart/post/strain_calculator.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- src/ansys/health/heart/post/strain_calculator.py
+++ src/ansys/health/heart/post/strain_calculator.py
@@ -320,7 +320,7 @@ class AhaStrainCalculator:
         Notes
         -----
         This function creates the 17-segment model for the left ventricle according
-        to the American Heart Association (AHA) [1]_
+        to the American Heart Association (AHA) [1]_.
 
         This method is modified from ``Matplotlibs`` `bullseye <https://matplotlib.org/stable/gallery/specialty_plots/leftventricle_bulleye.html>`_
         example. Copyright |copy| 2012- Matplotlib Development Team; All Rights Reserved.

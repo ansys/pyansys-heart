9133ba7c50df5270acfdd4fb728da961642817fd
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-06-25 13:03:34 +0200

    feat: use new load_model method in examples

--- examples/simulator/ep-simulator-fullheart_rodero.py
+++ examples/simulator/ep-simulator-fullheart_rodero.py
@@ -63,8 +63,9 @@ path_to_model, path_to_partinfo, _ = get_preprocessed_fullheart(resolution="2.0m
 # Load the full-heart model
 # ~~~~~~~~~~~~~~~~~~~~~~~~~
 # Load the full-heart model.
-model: models.FullHeart = models.FullHeart(working_directory=workdir)
-model.load_model_from_mesh(path_to_model, path_to_partinfo)
+model: models.FullHeart = models.HeartModel.load_model(
+    path_to_model, path_to_partinfo, working_directory=workdir
+)
 
 # Save the model.
 model.mesh.save(os.path.join(model.workdir, "simulation_model.vtu"))

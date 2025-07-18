4bce2b4de74c8dde5fa07305db2146bf25708993
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-07-18 10:41:10 +0200

    refactor: add TODO

--- src/ansys/health/heart/settings/defaults/mechanics.py
+++ src/ansys/health/heart/settings/defaults/mechanics.py
@@ -150,6 +150,7 @@ boundary_conditions = {
         "scale_factor": {"normal": 0.5, "radial": 1.0},
     },
     "end_diastolic_cavity_pressure": {
+        # TODO: align names with cavity/part names.
         # https://doi.org/10.3389/fphys.2018.00539
         "left_ventricle": Quantity(15, "mmHg"),
         "left_atrial": Quantity(15, "mmHg"),

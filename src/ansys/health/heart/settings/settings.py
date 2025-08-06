6ad121d23a871bf46e2cd2c02af293169fb5305c
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-08-06 08:59:07 +0200

    Revert "refactor: replace raise with warning"
    This reverts commit f33d5d71d8dee46a732220917d338293094a6f70.

--- src/ansys/health/heart/settings/settings.py
+++ src/ansys/health/heart/settings/settings.py
@@ -992,7 +992,7 @@ class DynaSettings:
         self.lsdyna_path: pathlib.Path = ls_dyna_abs_path
 
         if self.platform == "wsl" and os.name != "nt":
-            LOGGER.warning(f"""WSL is not supported on {os.name}.""")
+            raise WSLNotFoundError(f"""WSL is not supported on {os.name}.""")
 
         return
 

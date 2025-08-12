6a56dcf205f73c708f6cd44cb79257470d8ec2a4
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-08-12 15:33:34 +0200

    dbg: print info

--- src/ansys/health/heart/pre/mesher.py
+++ src/ansys/health/heart/pre/mesher.py
@@ -23,6 +23,7 @@
 """Module containing methods for interaction with Fluent meshing."""
 
 import glob
+import json
 import os
 from pathlib import Path
 import shutil
@@ -300,9 +301,12 @@ def _get_fluent_meshing_session(working_directory: str | Path) -> MeshingSession
                 **launch_config, **_extra_launch_kwargs, dry_run=True
             )
             container_dict["environment"]["FLUENT_ALLOW_REMOTE_GRPC_CONNECTION"] = "1"
-            container_dict["mount_source"] = working_directory
+            container_dict["mount_source"] = f"{working_directory}"
             container_dict["mount_target"] = "/mnt/pyfluent/meshing"
+
             launch_config["container_dict"] = container_dict
+            LOGGER.warning("Container config: {0}".format(json.dumps(container_dict, indent=2)))
+            LOGGER.warning("Launch config: {0}".format(json.dumps(launch_config, indent=2)))
 
             # set the UI mode to no GUI or graphics
             launch_config["ui_mode"] = pyfluent.UIMode.NO_GUI_OR_GRAPHICS

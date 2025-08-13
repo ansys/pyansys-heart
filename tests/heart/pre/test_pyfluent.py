08e487bbd267fb598e78411091dc7f57ac66f47e
mhoeijm <102799582+mhoeijm@users.noreply.github.com>
2025-08-13 10:07:43 +0200

    Revert "dbg: test_launch_fluent"
    This reverts commit 311b1fc0fd44b966ae6bf8640c4c0a57383ecedf.

--- tests/heart/pre/test_pyfluent.py
+++ tests/heart/pre/test_pyfluent.py
@@ -33,8 +33,9 @@ pytestmark = pytest.mark.requires_fluent
 def test_launch_fluent():
     """Launch pyfluent in meshing mode and check health."""
     try:
-        config_dict = pyfluent.launch_fluent(start_container=True, dry_run=True)
-        config_dict["environment"]["FLUENT_ALLOW_REMOTE_GRPC_CONNECTION"] = "1"
+        container_dict = {
+            "environment": {"FLUENT_ALLOW_REMOTE_GRPC_CONNECTION": "1"},
+        }
         session = pyfluent.launch_fluent(
             mode="meshing",
             precision="double",
@@ -42,8 +43,8 @@ def test_launch_fluent():
             start_transcript=False,
             ui_mode="no_gui",
             dimension=3,
-            container_dict=config_dict,
-            product_version="25.2",
+            container_dict=container_dict,
+            product_version="25.2",  # _get_supported_fluent_version(),
         )
         assert session.is_server_healthy()
         # try to initialize workflow

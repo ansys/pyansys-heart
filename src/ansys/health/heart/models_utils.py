bf0c8ae1debc4ee5317b5b5ab72bb4d943effcd1
wenfengye <wenfeng.ye@ansys.com>
2026-04-21 10:29:22 +0200

    safer surface merge

--- src/ansys/health/heart/models_utils.py
+++ src/ansys/health/heart/models_utils.py
@@ -31,7 +31,7 @@ import pyvista as pv
 from ansys.health.heart import LOG as LOGGER
 from ansys.health.heart.landmarks import LandMarks
 import ansys.health.heart.models as models
-from ansys.health.heart.objects import CapType, Point, SurfaceMesh
+from ansys.health.heart.objects import CapType, Point
 from ansys.health.heart.pre.conduction_path import ConductionPath, ConductionPathType
 
 
@@ -375,10 +375,8 @@ def define_full_conduction_system(
     left_bundle.down_path = left_purkinje
 
     # create complete endocardial surface for the right ventricle
-    endo_surface = SurfaceMesh(
-        model.right_ventricle.endocardium.merge(model.right_ventricle.septum),
-        name="right_ventricle_endo_septum",
-    )
+    surface_ids = [model.right_ventricle.endocardium.id, model.right_ventricle.septum.id]
+    endo_surface = model.mesh.get_surface(surface_ids)
 
     right_bundle = ConductionPath.create_from_keypoints(
         name=ConductionPathType.RIGHT_BUNDLE_BRANCH,
